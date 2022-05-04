a71dec731ebf810ca31a6c5b74291d083fefd04a
Kritika Rag <72866402+kritikarag@users.noreply.github.com>
2022-05-04 23:26:15 +0530

    Remove Duplicates from Sorted Array
    Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same.
    
    Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
    
    Return k after placing the final result in the first k slots of nums.
    
    Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.

---
+++ Array/Remove_Duplicates.cpp
@@ -0,0 +1,9 @@
+int removeDuplicates(vector<int>& nums) {
+        int count=0;
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]==nums[i-1])count++;
+            else nums[i-count]=nums[i];
+        }
+        nums.erase(nums.end()-count,nums.end());
+        return nums.size();
+    }
