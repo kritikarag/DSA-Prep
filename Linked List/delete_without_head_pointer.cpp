f0a12d2e17d20f5e8d71ef2068e8cd6179ed11ad
Kritika Rag <72866402+kritikarag@users.noreply.github.com>
2022-05-16 23:52:32 +0530

    Delete without head pointer
    You are given a pointer/ reference to the node which is to be deleted from the linked list of N nodes. The task is to delete the node. Pointer/ reference to head node is not given. 
    Note: No head reference is given to you. It is guaranteed that the node to be deleted is not a tail node in the linked list.

---
+++ Linked List/delete_without_head_pointer.cpp
@@ -0,0 +1,8 @@
+void deleteNode(Node *del)
+    {
+        del->data = del->next->data;
+        Node* temp1 = del->next;
+        Node* temp2 = del->next->next;
+        delete(temp1);
+        del->next=temp2;
+    }
