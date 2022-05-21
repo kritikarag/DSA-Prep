c39385767e82083945fd760526a36c2e5e6cb33b
Kritika Rag <72866402+kritikarag@users.noreply.github.com>
2022-05-21 23:27:55 +0530

    Maximum Product Subarray
    Given an integer array nums, find a contiguous non-empty subarray within the array that has the largest product, and return the product.
    
    The test cases are generated so that the answer will fit in a 32-bit integer.
    
    A subarray is a contiguous subsequence of the array.

---
+++ Dynamic Programming/max_product_subarray.cpp
@@ -0,0 +1,13 @@
+int maxProduct(vector<int>& nums) {
+        int res=nums[0];
+        for(int i=1,maxi=res,mini=res;i<nums.size();i++){
+            if(nums[i]<0)swap(mini,maxi);
+            
+            mini = min(nums[i],mini*nums[i]);
+            maxi = max(nums[i],maxi*nums[i]);
+            
+            res=max(res,maxi);
+        }
+        
+        return res;
+    }
