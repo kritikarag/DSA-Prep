63e3dad9912efd13ccd8956058ea00046a4e7e4b
Kritika Rag <72866402+kritikarag@users.noreply.github.com>
2022-05-27 23:44:46 +0530

    Update smallest_positive_missing.cpp

--- Heaps & PQs/smallest_positive_missing.cpp
+++ Heaps & PQs/smallest_positive_missing.cpp
@@ -1,7 +1,7 @@
 int missingNumber(int arr[], int n) 
     { 
         int res[n+1]={0};
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++ ){
             if(arr[i]>0 && arr[i]<=n){
                 res[arr[i]]++;
             }
