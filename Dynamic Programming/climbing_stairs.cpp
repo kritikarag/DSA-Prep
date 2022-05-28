f7a085a3d2f11aa20973ed8b5a51000310103cb1
Kritika Rag <72866402+kritikarag@users.noreply.github.com>
2022-05-28 23:29:46 +0530

    Update climbing_stairs.cpp

--- Dynamic Programming/climbing_stairs.cpp
+++ Dynamic Programming/climbing_stairs.cpp
@@ -1,7 +1,7 @@
 int climbStairs(int n) {
         if(n<=2)return n;
         
-        int prev1=1,prev2=2,res;
+        int prev1=1, prev2=2,res;
         for(int i=3;i<=n;i++){
             res=prev1+prev2;
             prev1=prev2;
