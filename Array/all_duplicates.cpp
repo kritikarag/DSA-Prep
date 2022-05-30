f10098bc9247a453d8f517cfe8d70692ed059b67
Kritika Rag <72866402+kritikarag@users.noreply.github.com>
2022-05-30 23:14:42 +0530

    Update all_duplicates.cpp

--- Array/all_duplicates.cpp
+++ Array/all_duplicates.cpp
@@ -1,7 +1,7 @@
 vector<int> findDuplicates(vector<int>& nums) {
         vector<int>ans;
         for(int i=0;i<nums.size();i++){
-            if(nums[abs(nums[i])-1]< 0)ans.push_back(abs(nums[i]));
+            if(nums[abs(nums[i])-1]< 0)ans.push_back( abs(nums[i]));
             else nums[abs(nums[i])-1] = -nums[abs(nums[i])-1];
         }
         return ans;
