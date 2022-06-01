cdba9eeeb12f087464703388f7e3ab6933f30b6d
Kritika Rag <72866402+kritikarag@users.noreply.github.com>
2022-06-01 23:46:56 +0530

    Add Binary
    Given two binary strings a and b, return their sum as a binary string.

---
+++ Mathematical_Problems/add_binary.cpp
@@ -0,0 +1,16 @@
+string addBinary(string a, string b) {
+        string res;
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+        while(i >= 0 || j >= 0){
+            int sum = carry;
+            if(i >= 0) sum += a[i--] - '0';
+            if(j >= 0) sum += b[j--] - '0';
+            carry = sum > 1 ? 1 : 0;
+            res += to_string(sum % 2);
+        }
+        if(carry) res += to_string(carry);
+        reverse(res.begin(), res.end());
+        return res;
+    }
