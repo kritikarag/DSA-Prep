35509ae406486eb4da89b97ee7ec7e283d324973
Kritika Rag <72866402+kritikarag@users.noreply.github.com>
2022-06-16 23:44:35 +0530

    Update 3sum.cpp

--- Array/3sum.cpp
+++ Array/3sum.cpp
@@ -10,7 +10,9 @@ vector<vector<int>> threeSum(vector<int>& nums) {
                    int sum =nums[i]+nums[l]+nums[r];
                    
                    if(sum<0) l++;
+                       
                    else if(sum>0)r--;
+                       
                    else {
                        res.push_back(vector<int>{nums[i],nums[l],nums[r]});
                        while(l+1<r && nums[l]==nums[l+1])l++;
