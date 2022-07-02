01d4dc7fde3e8649cd2aa92a3154a345851985e8
Kritika Rag <72866402+kritikarag@users.noreply.github.com>
2022-07-02 22:26:42 +0530

    Update Remove_Duplicates.cpp

--- Array/Remove_Duplicates.cpp
+++ Array/Remove_Duplicates.cpp
@@ -1,7 +1,7 @@
 int removeDuplicates(vector<int>& nums) {
         int count=0;
         for(int i=1;i<nums.size();i++){
-            if(nums[i]==nums[i-1])count++;
+            if(nums[i]==nums[i-1]) count++;
             else nums[i-count]=nums[i];
         }
         nums.erase(nums.end()-count,nums.end());
