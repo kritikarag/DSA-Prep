359964c4fc2dc1cdcd575ca0a159682cc4c4d651
Kritika Rag <72866402+kritikarag@users.noreply.github.com>
2022-07-06 23:54:07 +0530

    Update same_tree.cpp

--- Trees/same_tree.cpp
+++ Trees/same_tree.cpp
@@ -1,4 +1,5 @@
 bool isSameTree(TreeNode* p, TreeNode* q) {
         if (p == NULL || q == NULL) return (p == q);
+        
     return (p->val == q->val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right));
     }
