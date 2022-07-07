796739641d0e6f91ae3093ea3d77c71eca3f1840
Kritika Rag <72866402+kritikarag@users.noreply.github.com>
2022-07-07 06:46:05 +0530

    Symmetric Tree
    Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).

---
+++ Trees/symmetric_tree.cpp
@@ -0,0 +1,24 @@
+bool isSymmetric(TreeNode* root) {
+        queue<TreeNode*>check;
+        if(root==NULL)return true;
+        check.push(root->left);
+        check.push(root->right);
+        while(!check.empty()){
+            TreeNode* node1 = check.front();
+            check.pop();
+            TreeNode* node2 = check.front();
+            check.pop();
+            
+            if((!node1 && node2)||(!node2 && node1))return false;
+            
+            if(node1 && node2){
+                if(node1->val != node2->val)return false;
+                check.push(node1->left);
+                check.push(node2->right);
+                check.push(node2->left);
+                check.push(node1->right);
+            }
+            
+        }
+        return true;
+    }
