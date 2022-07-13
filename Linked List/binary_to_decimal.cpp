fd30bea73ca64466b3f7c0cdfc437f71ba53f01d
Kritika Rag <72866402+kritikarag@users.noreply.github.com>
2022-07-13 22:23:35 +0530

    Convert Binary Number in a Linked List to Integer
    Given head which is a reference node to a singly-linked list. The value of each node in the linked list is either 0 or 1. The linked list holds the binary representation of a number.
    
    Return the decimal value of the number in the linked list.

---
+++ Linked List/binary_to_decimal.cpp
@@ -0,0 +1,45 @@
+#Approach 1
+
+ListNode* reverse(ListNode* head){
+        ListNode* prev=NULL,*curr=head,*nxt = NULL;
+        while(curr){
+            nxt=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=nxt;
+        }
+        return prev;
+    }
+    int getDecimalValue(ListNode* head) {
+        if(!head)return 0;
+        if(!head->next)return head->val;
+        
+        ListNode* heads=reverse(head);
+        int count=0,res=0;
+        while(heads){
+            if(heads->val==1){
+                res+=pow(2,count);
+                count++;
+            }
+            else{
+                count++;
+            }
+            heads=heads->next;
+        }
+        return res;
+    }
+
+#Approach 2
+
+int getDecimalValue(ListNode* head) {
+        
+        int res = 0;
+        
+        while(head)
+        {
+            res = res*2 + head->val;
+            head = head->next;
+        }
+        
+        return res;
+    }
