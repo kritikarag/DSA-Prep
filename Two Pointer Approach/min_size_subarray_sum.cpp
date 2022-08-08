264d137ae3a351007a44b826c69657c717c79784
[Kritika <kritikarag.kr@gmail.com>
2022-08-08 09:54:35 +0530

    new file:   Two Pointer Approach/min_size_subarray_sum.cpp

---
+++ Two Pointer Approach/min_size_subarray_sum.cpp
@@ -0,0 +1,22 @@
+int minSubArrayLen(int target, vector<int> &nums)
+{
+    int n = nums.size();
+    int i = 0, j = 0, sum = 0;
+    int len = INT_MAX;
+    map<int, int> mp;
+    for (int i = 0; i < n; i++)
+    {
+        sum += nums[i];
+        if (sum >= target)
+        {
+            len = min(len, i - j + 1);
+            while (sum >= target)
+            {
+                len = min(len, i - j + 1);
+                sum -= nums[j++];
+            }
+        }
+    }
+
+    return (len == INT_MAX) ? 0 : len;
+}
