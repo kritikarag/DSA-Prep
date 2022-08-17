c7d83bf3191a2bdbb4441ecf5b96ec469a102fa5
[Kritika <kritikarag.kr@gmail.com>
2022-08-17 11:41:14 +0530

    modified:   Linked List/reverse_ll.cpp

--- Linked List/reverse_ll.cpp
+++ Linked List/reverse_ll.cpp
@@ -1,10 +1,23 @@
+//Iterative Approach
 ListNode* reverseList(ListNode* head) {
-        ListNode* prev=NULL, *curr=head, *next=NULL;
-        while(curr){
-            next=curr->next;
-            curr->next=prev;
-            prev = curr;
-            curr=next;
-        }
-        return prev;
+    ListNode* prev=NULL, *curr=head, *next=NULL;
+    while(curr){
+        next=curr->next;
+        curr->next=prev;
+        prev = curr;
+        curr=next;
     }
+    return prev;
+}  
+
+//Recursive Approach
+ListNode* reverseList(ListNode* head){
+    if(head==NULL||head->next==NULL)
+        return head;
+    
+    ListNode* start = reverseList(head->next);
+    head->next->next=head;
+    head->next=NULL;
+
+    return start;
+}
