0212bd8b9805dd3eea73253ac111b967977aaee7
Kritika Rag <kritikarag.kr@gmail.com>
2022-08-27 17:03:17 +0530

    modified:   Array/sort_colors.cpp

--- Array/sort_colors.cpp
+++ Array/sort_colors.cpp
@@ -1,3 +1,4 @@
+//1st APPROACH
 void sortColors(vector<int>& nums) {
         int zero=0,one=0,two=0;
         for(int i=0;i<nums.size();i++){
@@ -11,3 +12,17 @@ void sortColors(vector<int>& nums) {
         while(one--)nums[j++]=1;
         while(two--)nums[j++]=2;
     }
+
+//2ND APPROACH
+
+void sortColors(vector<int> &nums)
+{
+    int k = 0, j = nums.size() - 1, i = 0;
+    for (int i = 0; i <= j; i++)
+    {
+        if (nums[i] == 0)
+            swap(nums[i], nums[k++]);
+        if (nums[i] == 2)
+            swap(nums[i--], nums[j--]);
+    }
+}
