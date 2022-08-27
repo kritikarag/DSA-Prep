477a3aaf0db94294fa657230bcd15d5417fba318
Kritika Rag <kritikarag.kr@gmail.com>
2022-08-28 03:13:37 +0530

    modified:   Array/zero_sum_longest_subarray.cpp

--- Array/zero_sum_longest_subarray.cpp
+++ Array/zero_sum_longest_subarray.cpp
@@ -5,10 +5,11 @@ int maxLen(vector<int> &arr, int n)
 {
     map<int, int> mp;
     int curr = 0, len = 0;
-    mp[0] = -1;
     for (int i = 0; i < n; i++)
     {
         curr += arr[i];
+        if (curr == 0)
+            len = max(len, i + 1);
         if (mp.find(curr) != mp.end())
         {
             len = max(len, i - mp[curr]);
