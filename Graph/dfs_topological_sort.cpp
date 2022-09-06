d4e56e97de4b111ff8b403d577d0cd4eb981ddd3
Kritika Rag <kritikarag.kr@gmail.com>
2022-09-06 21:06:02 +0530

    new file:   Graph/dfs_topological_sort.cpp

---
+++ Graph/dfs_topological_sort.cpp
@@ -0,0 +1,54 @@
+vector<int>topologicalSortDFS(int v, vector<int>adj[]){
+    vector<int>vis(v+1,0);
+    vector<int>ts;
+    stack<int> st;
+    for(int i=1;i<=v;i++){
+        if(!vis[i]){
+            vis[i]=1;
+            for(auto it: adj[i]){
+                if(!vis[it]){
+                    vis[it]=1;
+                    st.push(it);
+                }
+            }
+            st.push(i);
+        }
+    }
+
+    while(!st.empty()){
+        ts.push_back(st.top());
+        st.pop();
+    }
+}
+
+//USING RECURSION
+
+void findtopo(int node, vector<int>&vis,stack<int>&st,vector<int>adj[]){
+    vis[node]=1;
+    for(auto it: adj[node]){
+        if(!vis[it]){
+            findtopo(it, vis, st,adj);
+        }
+    }
+    st.push(node);
+}
+
+vector<int> topologicalSortDFS(int v, vector<int> adj[])
+{
+    vector<int> vis(v + 1, 0);
+    vector<int> ts;
+    for (int i = 1; i <= v; i++)
+    {
+        if (!vis[i])
+        {
+            findtopo(i,vis,st,adj);
+        }
+    }
+
+    while(!st.empty()){
+        ts.push_back(st.top());
+        st.pop();
+    }
+
+    return ts;
+}
