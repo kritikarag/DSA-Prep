713d686230d686b3ef43c6f264c30df8a92c6aa1
Kritika Rag <kritikarag.kr@gmail.com>
2022-09-06 14:05:23 +0530

    modified:   Graph/basics.cpp

--- Graph/basics.cpp
+++ Graph/basics.cpp
@@ -53,3 +53,66 @@ int main()
 
     return 0;
 }
+
+// BFS OF GRAPH
+
+vector<int> bfsofGraph(int v, vector<int> adj[])
+{
+    vector<int> bfs;
+    vector<int> vis(v + 1, 0);
+
+    for (int i = 1; i <= v; i++)
+    {
+        if (!vis[i])
+        {
+            queue<int> q;
+            q.push(i);
+            vis[i] = 1;
+            while (!q.empty())
+            {
+                int node = q.front();
+                q.pop();
+                bfs.push_back(node);
+                for (auto it : adj[node])
+                {
+                    if (!vis[it])
+                        q.push(it);
+                    vis[it] = 1;
+                }
+            }
+        }
+    }
+    return bfs;
+}
+
+// DFS OF GRAPH
+void dfstraversal(int node, vector<int> &vis, vector<int> adj[], vector<int> &dfs)
+{
+    dfs.push_back(node);
+    vis[node] = 1;
+    for (auto it : adj[node])
+    {
+        if (!vis[it])
+        {
+            dfstraversal(it, vis, adj, bfs);
+        }
+    }
+}
+vector<int> dfsofGraph(int v, vector<int> &adj[])
+{
+    vector<int> dfs;
+    vector<int> vis(v + 1, 0);
+
+    for (int i = 1; i <= v; i++)
+    {
+        if (!vis[i])
+        {
+            dfstraversal(i, vis, adj, dfs);
+        }
+    }
+    return dfs;
+}
+
+void cycleDetectionBFS(int v, vector<int> adj[])
+{
+}
