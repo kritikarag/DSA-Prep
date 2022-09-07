53a78c87d81c3d8425030ff0ab3922ff78572c7b
Kritika Rag <kritikarag.kr@gmail.com>
2022-09-07 23:46:25 +0530

    modified:   Dynamic Programming/climbing_stairs.cpp

--- Dynamic Programming/climbing_stairs.cpp
+++ Dynamic Programming/climbing_stairs.cpp
@@ -8,4 +8,16 @@ int climbStairs(int n) {
             prev2=res;
         }
         return res;
+}
+
+int climbStairs(int n){
+    vector<int>dp(n+1,0);
+    dp[1]=1;
+    dp[2]=2;
+
+    for(int i=3;i<=n;i++){
+        dp[i]=dp[i-1]+dp[i-2];
     }
+
+    return dp[n];
+}
