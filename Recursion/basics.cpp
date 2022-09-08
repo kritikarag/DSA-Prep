dfa3a2cc5b27253dc87d7d2e888569608870b2f2
Kritika Rag <kritikarag.kr@gmail.com>
2022-09-08 17:03:35 +0530

    modified:   Recursion/basics.cpp

--- Recursion/basics.cpp
+++ Recursion/basics.cpp
@@ -34,3 +34,32 @@ void print(int i, int n){
     cout<<i<<endl;
 }
 print(1,N);
+
+//REVERSE AN ARRAY
+
+void reverse(int l, int r, int &arr[])
+{
+    if (l >= r)
+        return;
+    swap(arr[l], arr[r]);
+    reverse(l + 1, r - 1, arr);
+}
+
+void reverse(int i, int n, int &arr[])
+{
+    if (i >= n / 2)
+        return;
+
+    swap(arr[i], arr[n - i - 1]);
+    reverse(i + 1, n, arr);
+}
+
+// PALINDROME STRING
+
+bool check(int i, int n, string &str){
+    if(l>=n/2)return true;
+
+    if(str[i]!=str[n-i-1])return false;
+
+    check(i+1,n,str);
+}
