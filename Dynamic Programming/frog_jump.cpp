7d04df9ce0dabaa3f74a4073dc7a64ae1c76befb
Kritika Rag <kritikarag.kr@gmail.com>
2022-09-08 12:57:30 +0530

    new file:   Dynamic Programming/frog_jump.cpp

---
+++ Dynamic Programming/frog_jump.cpp
@@ -0,0 +1,63 @@
+//RECURSION
+
+int frogJump(int n, int energy[]){
+    if(n==0)return 0;
+
+    int step1 = frogJump(n-1,energy)+ (energy[i]-energy[i-1]);
+    if(n>1)
+        int step2 = frogJump(n-2,energy)+(energy[i]-energy[i-2]);
+
+    return min(step1,step2);
+}
+
+//MEMORIZATION
+
+int frogJump(int n, int energy[],vector<int>&dp)
+{
+    if (n == 0)
+        return 0;
+    if(dp[n]!=-1)return dp[n];
+    int step1 = frogJump(n - 1,energy,dp) + abs(energy[i] - energy[i - 1]);
+    if(n>1)
+        int step2 = frogJump(n - 2,energy,dp) + abs(energy[i] - energy[i - 2]);
+
+    return dp[n] = min(step1, step2);
+}
+
+//TABULATION
+
+int frogJump(int n, int energy[])
+{
+    vector<int>dp(n,INT_MAX);
+    dp[0]=0;
+
+    for(int i=1;i<n;i++){
+        int step1 = dp[n - 1] + (energy[i] - energy[i - 1]);
+        step2=INT_MAX;
+        if(n>1)
+            int step2 = dp[n - 2] + (energy[i] - energy[i - 2]);
+
+        dp[i] min(step1, step2);
+    }
+
+    return dp[n-1];
+}
+
+//SPACE OPTIMIZATION
+
+int frogJump(int n, int energy[])
+{
+    int prev1=0,prev2=0;
+
+    for(int i=1;i<n;i++){
+        int step1 = prev1 + (energy[i] - energy[i - 1]);
+        if (n > 1)
+            int step2 = prev2 + (energy[i] - energy[i - 2]);
+
+        int curr = min(step1, step2);
+        prev2 = prev1;
+        prev1 = curr;
+    }
+
+    return prev1;
+}
