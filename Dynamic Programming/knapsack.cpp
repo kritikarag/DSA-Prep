b8ea72389a2c83e05ea52581314139c718d4b18c
Kritika Rag <kritikarag.kr@gmail.com>
2022-09-09 08:44:40 +0530

    modified:   Dynamic Programming/knapsack.cpp

--- Dynamic Programming/knapsack.cpp
+++ Dynamic Programming/knapsack.cpp
@@ -20,13 +20,13 @@ int knapsack(int arr[],int wt[],int w,int n){
         dp[0][i] = 0;
     for (int i = 0; i <= n; i++)
         dp[i][0] = 1;
-    for(inti=1;i<n+1;i++)
+    for(inti=1;i<n+1;i++){
         for(int j=1;j<w+1;j++)
             if(wt[i-1]<=j){
                 dp[i][j]=max(arr[i-1]+dp[i-1][j-wt[i-1]],dp[i-1][j]);
             }
             else dp[i][j]=dp[i-1][j];
-
+    }
     return dp[n][w];
 }
 
