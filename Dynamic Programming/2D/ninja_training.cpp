d5d8338903c95fa66a5b685312c08a643d0af181
Kritika Rag <kritikarag.kr@gmail.com>
2022-09-10 04:16:42 +0530

    new file:   Dynamic Programming/2D/ninja_training.cpp

---
+++ Dynamic Programming/2D/ninja_training.cpp
@@ -0,0 +1,10 @@
+int maxenergy(vector<vector<int>>&vec){
+    int n = vec.size();
+    int m = vec[0].size();
+    vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            
+        }
+    }
+}
