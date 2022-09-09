19be3650cf176ed04ae4a4a8d969138aeebbc14f
Kritika Rag <kritikarag.kr@gmail.com>
2022-09-10 01:32:07 +0530

    new file:   Dynamic Programming/DP On String/lc_substring_len.cpp

---
+++ Dynamic Programming/DP On String/lc_substring_len.cpp
@@ -0,0 +1,92 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//MEMORIZATION
+
+int solve(string s1, string s2, int ind1, int ind2, vector<vector<int>> &dp)
+{
+
+    if (ind1 == 0 || ind2 == 0)
+        return 0;
+    if (dp[ind1][ind2] != -1)
+        return dp[ind1][ind2];
+    if (s1[ind1 - 1] != s2[ind2 - 1])
+    {
+        return dp[ind1][ind2] = 0;
+    }
+    else
+    {
+        return dp[ind1][ind2] = 1 + solve(s1, s2, ind1 - 1, ind2 - 1, dp);
+    }
+}
+
+int maxlength(int n, int m, string &a, string &b){
+    int maxi = INT_MIN;
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            maxi = max(maxi, solve(a, b, i + 1, j + 1, dp));
+        }
+    }
+    return maxi;
+}
+
+//TABULATION
+
+int maxlength(int n, int m,string &a, string &b){
+    vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+    int maxi = 0;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            if(a[i-1]==b[j-1]){
+                dp[i][j] = 1 + dp[i-1][j-1];
+                maxi = max(maxi,dp[i][j]);
+            }
+            else{
+                dp[i][j] = 0;
+            }
+        }
+    }
+    return maxi;
+}
+
+// SPACE OPTIMIZATION
+
+int maxlength(int n, int m, string &a, string &b)
+{
+    vector<int>prev(n+1,0);
+    int maxi = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        vector<int>curr(m+1,0);
+        for (int j = 1; j <= m; j++)
+        {
+            if (a[i - 1] == b[j - 1])
+            {
+                curr[j] = 1 + prev[j-1];
+                maxi = max(maxi, curr[j]);
+            }
+            else
+            {
+                curr[j] = 0;
+            }
+        }
+        prev = curr;
+    }
+    return maxi;
+}
+
+int main()
+{
+
+    string s1 = "abcjklp";
+    string s2 = "acjkp";
+    int n = s1.length();
+    int m = s2.length();
+
+    cout << "The Length of Longest Common Substring is " << maxlength(n, m, s1, s2)<<endl;
+    return 0;
+}
