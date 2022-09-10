3a3dad75bf1336bf321a512c6a6b805bb1391a6f
Kritika Rag <kritikarag.kr@gmail.com>
2022-09-10 14:21:42 +0530

    modified:   Dynamic Programming/2D/min_sum_path_matrix.cpp

--- Dynamic Programming/2D/min_sum_path_matrix.cpp
+++ Dynamic Programming/2D/min_sum_path_matrix.cpp
@@ -1,47 +1,80 @@
 // https://leetcode.com/problems/minimum-path-sum/
-int minPathSum(vector<vector<int>> &grid)
+#include<bits/stdc++.h>
+using namespace std;
+
+//RECUSION
+
+int minPathSum(int i, int j, vector<vector<int>> &grid)
 {
-    int m = grid.size();
-    int n = grid[0].size();
-    vector<vector<int>> dp(m, vector<int>(n, 0));
+    if(i<=0||j<=0)return INT_MAX;
+    if(i==1 && j==1)return grid[i-1][j-1];
 
-    for (int i = 0; i < m; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            dp[i][j] = grid[i][j];
-            if (i == 0 && j == 0)
-                continue;
-            else if (i == 0)
-                dp[i][j] += dp[i][j - 1];
-            else if (j == 0)
-                dp[i][j] += dp[i - 1][j];
-            else
-                dp[i][j] += min(dp[i - 1][j], dp[i][j - 1]);
-        }
-    }
-    return dp[m - 1][n - 1];
+    return grid[i-1][j-1]+min(minPathSum(i,j-1,grid),minPathSum(i-1,j,grid));
 }
 
-//Recursion
-
-int minpathsum(int i, int j){
-    if(i>=n||j>=m)return INT_MAX;
-    if(i==n-1 && j==m-1)return a[n-1][m-1];
-
-    return grid[i][j]+min(minpathsum(i,j+1),minpathsum(i+1,j));
-}
+//MEMORIZATION
 
 int dp[n][m]={-1};
-int minpathsum(int i, int j)
+int minpathsum(int i, int j,vector<vector<int>>&grid,vector<vector<int>>&dp)
 {
-    if (i >= n || j >= m)
+    if (i <= 0 || j <= 0)
         return INT_MAX;
-    if (i == n - 1 && j == m - 1)
-        return a[n - 1][m - 1];
+    if (i == 1 && j == 1)
+        return grid[n - 1][m - 1];
 
     if(dp[i][j]!=-1)return dp[i][j];
 
-    dp[i][j] = grid[i][j] + min(minpathsum(i, j + 1), minpathsum(i + 1, j));
+    dp[i][j] = grid[i-1][j-1] + min(minpathsum(i, j - 1,grid,dp), minpathsum(i - 1, j,grid,dp));
     return dp[i][j];
+}
+
+// //TABULATION
+
+int minPathSum(int m, int n, vector<vector<int>> &grid)
+{ 
+    vector<vector<int>> dp(m + 1, vector<int>(n + 1, INT_MAX));
+
+    for (int i = 1; i <= m; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            if (i == 1 && j == 1)
+                dp[i][j] = grid[i - 1][j - 1];
+            else
+                dp[i][j] = grid[i - 1][j - 1] + min(dp[i - 1][j], dp[i][j - 1]);
+        }
+    }
+    return dp[m][n];
+}
+
+// //SPACE OPTIMIZATION
+int minPathSum(int m, int n, vector<vector<int>> &grid)
+{ 
+    vector<int> prev(m + 1, INT_MAX);
+
+    for (int i = 1; i <= m; i++)
+    {
+        vector<int> curr(n + 1, INT_MAX);
+        for (int j = 1; j <= n; j++)
+        {
+            if (i == 1 && j == 1)
+                curr[j] = grid[i-1][j-1];
+            else
+                curr[j] = grid[i-1][j-1] + min(prev[j],curr[j - 1]);
+        }
+        prev = curr;
+    }
+    return prev[n];
+}
+
+int main()
+{
+
+    vector<vector<int>> matrix{{5, 9, 6},
+                               {11, 5, 2}};
+
+    int n = matrix.size();
+    int m = matrix[0].size();
+
+    cout << minPathSum(n, m, matrix);
 }
