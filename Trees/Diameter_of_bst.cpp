9787b77178b48297662521cf8e3811fc2ff184c1
Kritika Rag <kritikarag.kr@gmail.com>
2022-09-11 20:33:37 +0530

    modified:   Trees/Diameter_of_bst.cpp

--- Trees/Diameter_of_bst.cpp
+++ Trees/Diameter_of_bst.cpp
@@ -4,10 +4,10 @@ int height(TreeNode* root,int &res){
         int r = height(root->right,res);
         res = max(res,l+r);
         return 1+max(l,r);
-    }
-    int diameterOfBinaryTree(TreeNode* root) {
-        if(!root)return 0;
-        int res=0;
-        height(root,res);
-        return res;
-    }
+}
+int diameterOfBinaryTree(TreeNode* root) {
+    if(!root)return 0;
+    int res=0;
+    height(root,res);
+    return res;
+}
