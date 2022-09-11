94121edd379a18c2a64dc01764e0182849ab8008
Kritika Rag <kritikarag.kr@gmail.com>
2022-09-12 05:13:01 +0530

    new file:   Trees/top_view_bst.cpp

---
+++ Trees/top_view_bst.cpp
@@ -0,0 +1,33 @@
+struct TreeNode{
+    int val;
+    TreeNode* left, *right;
+    TreeNode(int x){
+        val = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+vector<int>topView(TreeNode* root){
+    vector<int>ans;
+    map<int,int>mp;
+
+    queue<pair<TreeNode*,int>>q;
+    q.push({root,0});
+    mp[0] = root->val;
+    while(!q.empty()){
+        auto curr = q.front();
+        q.pop();
+        TreeNode* temp = curr.first;
+        int col = curr.second;
+        if(mp.find(line)==mp.end())mp[line] = temp->val;
+
+        if(temp->left)q.push({temp->left,line-1});
+        if(temp->right)q.push({temp->right,line+1});
+    }
+
+    for(auto it: mp){
+        ans.push_back(it.second);
+    }
+
+    return ans;
+}
