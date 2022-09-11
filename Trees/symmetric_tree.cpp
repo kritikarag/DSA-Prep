51371688c7efc6597fb72427d4a35a5657ea52c2
Kritika Rag <kritikarag.kr@gmail.com>
2022-09-12 05:20:25 +0530

    modified:   Trees/symmetric_tree.cpp

--- Trees/symmetric_tree.cpp
+++ Trees/symmetric_tree.cpp
@@ -22,3 +22,17 @@ bool isSymmetric(TreeNode* root) {
         }
     return true;
 }
+
+
+//RECURSION
+
+bool check(TreeNode* root1, TreeNode* root2){
+    if(!root1||!root2)return root1==root2;
+
+    return (root1->val==root2->val)&&check(root1->left,root2->right)&&check(root1->right,root2->left);
+}
+bool isSymmetriic(TreeNode* node){
+    if(root==NULL)return true;
+
+    return check(root->left,root->right);
+}
