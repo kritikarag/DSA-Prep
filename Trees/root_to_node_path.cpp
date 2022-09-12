53e59fa70f14bda5b1e7b175dadf96b8a5ceb778
Kritika Rag <kritikarag.kr@gmail.com>
2022-09-12 05:40:53 +0530

    new file:   Trees/root_to_node_path.cpp

---
+++ Trees/root_to_node_path.cpp
@@ -0,0 +1,15 @@
+bool getPath(Node* root, vector<int>&res,int x){
+    if(!root)return false;
+
+    res.push_back(root->data);
+
+    if(root->data==x)return true;
+
+    if(getPath(root->left, res,x)||getPath(root->right,res, x))return true;
+
+    res.pop_back();
+    return false;
+
+}
+
+//Print resultant array in the main function
