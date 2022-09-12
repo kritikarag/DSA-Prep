2f0985408d391e4c53b2387c7b474d54c1596ba7
Kritika Rag <kritikarag.kr@gmail.com>
2022-09-12 05:42:43 +0530

    modified:   Trees/root_to_node_path.cpp

--- Trees/root_to_node_path.cpp
+++ Trees/root_to_node_path.cpp
@@ -1,15 +1,18 @@
-bool getPath(Node* root, vector<int>&res,int x){
-    if(!root)return false;
+bool getPath(Node *root, vector<int> &res, int x)
+{
+    if (!root)
+        return false;
 
     res.push_back(root->data);
 
-    if(root->data==x)return true;
+    if (root->data == x)
+        return true;
 
-    if(getPath(root->left, res,x)||getPath(root->right,res, x))return true;
+    if (getPath(root->left, res, x) || getPath(root->right, res, x))
+        return true;
 
     res.pop_back();
     return false;
-
 }
 
 //Print resultant array in the main function
