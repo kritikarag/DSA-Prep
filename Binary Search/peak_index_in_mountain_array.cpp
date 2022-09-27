372d6ca7b8fe2bc34e42bea0e99f73e8dbe1dbd9
Kritika Rag <kritikarag.kr@gmail.com>
2022-09-27 19:45:10 +0530

    modified:   Binary Search/peak_index_in_mountain_array.cpp

--- Binary Search/peak_index_in_mountain_array.cpp
+++ Binary Search/peak_index_in_mountain_array.cpp
@@ -20,3 +20,30 @@ int peakIndexInMountainArray(vector<int> &arr)
     }
     return -1;
 }
+
+
+//Another Binary Search Approach
+
+int peakIndexInMountainArray(vector<int> &arr)
+{
+    int low = 0, high = arr.size() - 1;
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        if (arr[mid] < arr[mid + 1])
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+    return low;
+}
+
+// Iterative Search Method
+
+int peakIndexInMountainArray(vector<int> &arr)
+{
+    int left = 0;
+    while (arr[left] < arr[left + 1])
+        left++;
+    return left;
+}
