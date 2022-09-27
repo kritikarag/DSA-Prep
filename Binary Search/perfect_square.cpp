b5d02a555d8fff04b6a7ab094ccad7f907c596b7
Kritika Rag <kritikarag.kr@gmail.com>
2022-09-28 03:44:28 +0530

    new file:   Binary Search/perfect_square.cpp

---
+++ Binary Search/perfect_square.cpp
@@ -0,0 +1,35 @@
+// https://leetcode.com/problems/valid-perfect-square/
+
+//METHOD 1
+bool isPerfectSquare(int num)
+{
+    int low = 1, high = num;
+    while (low <= high)
+    {
+        long long int mid = low + (high - low) / 2;
+        if (mid * mid == num)
+            return true;
+        else if (mid * mid < num)
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+
+    return false;
+}
+
+//METHOD 2
+bool isPerfectSquare(int num)
+{
+    int a = 1;
+    while (num > 0)
+    {
+        num = num - a;
+        a = a + 2;
+    }
+    return num == 0;
+}
