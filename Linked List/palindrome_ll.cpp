96155b4a348645aa38a8d7a6d18528856033df3a
Kritika Rag <kritikarag.kr@gmail.com>
2022-10-01 18:13:05 +0530

    modified:   Linked List/palindrome_ll.cpp

--- Linked List/palindrome_ll.cpp
+++ Linked List/palindrome_ll.cpp
@@ -1,35 +1,35 @@
 ListNode* reverse(ListNode*head){
-        ListNode* prev=NULL, *curr= head, *next = NULL;
-        while(curr){
-            next = curr->next;
-            curr->next=prev;
-            prev = curr;
-            curr = next;  
+    ListNode* prev=NULL, *curr= head, *next = NULL;
+    while(curr){
+        next = curr->next;
+        curr->next=prev;
+        prev = curr;
+        curr = next;  
+    }
+    return prev;
+}
+    
+bool isPalindrome(ListNode* head) {
+    ListNode*slow=head;
+    ListNode*fast=head;
+    ListNode*x=head;
+    while(fast and fast->next){
+        slow=slow->next;
+        fast=fast->next->next;;
+        
+    }
+    ListNode*heads=reverse(slow);
+    while(head !=NULL and heads!=NULL){
+        if(head->val!=heads->val){
+            break;
         }
-        return prev;
+        head=head->next;
+        heads=heads->next;
+        
     }
     
-    bool isPalindrome(ListNode* head) {
-        ListNode*slow=head;
-        ListNode*fast=head;
-        ListNode*x=head;
-        while(fast and fast->next){
-            slow=slow->next;
-            fast=fast->next->next;;
-            
-        }
-       ListNode*heads=reverse(slow);
-        while(head !=NULL and heads!=NULL){
-            if(head->val!=heads->val){
-                break;
-            }
-            head=head->next;
-            heads=heads->next;
-            
-        }
-        
-        if(head==NULL || heads==NULL){
-            return true;
-        }
-        return false;
+    if(head==NULL || heads==NULL){
+        return true;
     }
+    return false;
+}
