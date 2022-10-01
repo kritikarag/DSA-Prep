536726e8bf9adb0d2d29c52a21904579ef96f95f
Kritika Rag <kritikarag.kr@gmail.com>
2022-10-01 20:42:45 +0530

    modified:   Linked List/linked_list_cycle.cpp

--- Linked List/linked_list_cycle.cpp
+++ Linked List/linked_list_cycle.cpp
@@ -8,25 +8,3 @@ bool hasCycle(ListNode *head) {
     
     return false;
 }
-
-//Optimized Approach
-
-ListNode *detectCycle(ListNode *head)
-{
-    ListNode *fast = head, *slow = head, *entry = head;
-    while (fast && fast->next)
-    {
-        slow = slow->next;
-        fast = fast->next->next;
-        if (slow == fast)
-        {
-            while (entry != slow)
-            {
-                entry = entry->next;
-                slow = slow->next;
-            }
-            return entry;
-        }
-    }
-    return NULL;
-}
