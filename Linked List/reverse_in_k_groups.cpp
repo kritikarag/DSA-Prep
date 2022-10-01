7a1d508f6d9b52a496a90e12bebe34234f4b524b
Kritika Rag <kritikarag.kr@gmail.com>
2022-10-01 19:14:25 +0530

    modified:   Linked List/reverse_in_k_groups.cpp

--- Linked List/reverse_in_k_groups.cpp
+++ Linked List/reverse_in_k_groups.cpp
@@ -1,10 +1,9 @@
 struct ListNode{
     int val;
     ListNode * next;
-    ListNode(int data){
-        val = data;
-        next = NULL;
-    }
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next)
 }
 ListNode* reverseKGroup(ListNode* head, int k) {
     if(head==NULL||k==1)return head;
