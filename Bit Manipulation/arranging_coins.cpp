a3ffcc576f0c869d8438a034915e6a0fc10f69c5
Kritika Rag <kritikarag.kr@gmail.com>
2022-10-04 06:28:31 +0530

    new file:   Bit Manipulation/arranging_coins.cpp

---
+++ Bit Manipulation/arranging_coins.cpp
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/arranging-coins/
+
+int arrangeCoins(int n)
+{
+    int mask = 1 << 15;
+    long result = 0;
+    while (mask)
+    {
+        result |= mask;
+        if ((result * (result + 1)) / 2 > n)
+        {
+            result ^= mask;
+        }
+        mask >>= 1;
+    }
+    return (int)result;
+}
