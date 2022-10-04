b6bc9a45e63c2ba95608c196470a736740c9b74e
Kritika Rag <kritikarag.kr@gmail.com>
2022-10-05 05:00:13 +0530

    modified:   Matrix Problem/k_weakest_row.cpp

--- Matrix Problem/k_weakest_row.cpp
+++ Matrix Problem/k_weakest_row.cpp
@@ -31,3 +31,21 @@ vector<int> kWeakestRows(vector<vector<int>> &mat, int k)
 
     return res;
 }
+
+//2ND APPROACH
+
+vector<int> kWeakestRows(vector<vector<int>> &mat, int k)
+{
+    int n = mat[0].size();
+    for (int i = 0; i < mat.size(); i++)
+    {
+        mat[i].push_back(i);
+    }
+    sort(mat.begin(), mat.end());
+    vector<int> ans(k);
+    for (int i = 0; i < k; i++)
+    {
+        ans[i] = mat[i][n];
+    }
+    return ans;
+}
