0ef4849a8b7d9f94968290e356fab7e8ead815c5
Kritika Rag <kritikarag.kr@gmail.com>
2022-10-05 07:09:09 +0530

    modified:   Binary Search/sum_of_square_numbers.cpp

--- Binary Search/sum_of_square_numbers.cpp
+++ Binary Search/sum_of_square_numbers.cpp
@@ -33,3 +33,28 @@ bool judgeSquareSum(int c)
 
     return false;
 }
+
+
+//Method 2
+
+bool judgeSquareSum(int c)
+{
+    if (c < 3)
+        return true;
+    long long int a = 0, b = sqrt(c);
+    while (a <= b)
+    {
+        if (a * a + b * b == c)
+        {
+            return true;
+        }
+        else if (a * a + b * b < c)
+        {
+            a++;
+        }
+        else
+        {
+            b--;
+        }
+    }
+    return false;
