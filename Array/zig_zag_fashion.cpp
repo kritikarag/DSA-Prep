17aa463e1b1231018b57f14c2aecbb412a5d169a
Kritika Rag <kritikarag.kr@gmail.com>
2022-10-10 02:30:58 +0530

    new file:   Array/zig_zag_fashion.cpp

---
+++ Array/zig_zag_fashion.cpp
@@ -0,0 +1,24 @@
+void zigZag(int arr[], int n)
+{
+    // flag true mean '<' is expected otherwise '>' is expected
+    bool flag = true;
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (flag)
+        {
+            if (arr[i] > arr[i + 1])
+            {
+                swap(arr[i], arr[i + 1]);
+            }
+        }
+        else
+        {
+            if (arr[i + 1] > arr[i])
+            {
+                swap(arr[i], arr[i + 1]);
+            }
+        }
+
+        flag = !flag;
+    }
+}
