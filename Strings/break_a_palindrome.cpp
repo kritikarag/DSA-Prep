109d9a1ab9d660d918e7690ec564b2e7dde34501
Kritika Rag <kritikarag.kr@gmail.com>
2022-10-10 17:27:17 +0530

    new file:   Strings/break_a_palindrome.cpp

---
+++ Strings/break_a_palindrome.cpp
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/break-a-palindrome/
+
+string breakPalindrome(string S)
+{
+    int n = S.size();
+    for (int i = 0; i < n / 2; ++i)
+    {
+        if (S[i] != 'a')
+        {
+            S[i] = 'a';
+            return S;
+        }
+    }
+    S[n - 1] = 'b';
+    return n < 2 ? "" : S;
+}
