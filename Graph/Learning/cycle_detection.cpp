8064977e18374d790bb7b519e302ccc6666fc066
Kritika Rag <kritikarag.kr@gmail.com>
2022-10-11 06:02:01 +0530

    new file:   Graph/Learning/cycle_detection.cpp

---
+++ Graph/Learning/cycle_detection.cpp
@@ -0,0 +1,56 @@
+Using BFS
+
+bool cycleDetectionBFS(int v, vector<int> adj[])
+{
+    queue<int,int>q;
+    vector<int>vis(n+1,0);
+
+    for(int i=0;i<=n;i++){
+        if(!vis[i]){
+            q.push({i,-1});
+            vis[i]=1;
+            while(!q.empty()){
+                int node = q.front();
+                q.pop();
+
+                for(auto it: adj[node.first]){
+                    if(vis[it]){
+                        if(it!=node.second)return true;
+                    }
+                    else{
+                        q.push({it,node.first});
+                        vis[it] = 1;
+                    }
+                }
+            }
+        }
+    }
+    return false;
+}
+
+USING DFS
+
+bool(vector<int>adj[], int node, int par, vector<int>&vis){
+    vis[node]=1;
+    for(auto it: adj[node]){
+        if(vis[it]){
+            if(it!=par)return true;
+        }
+        else{
+            if(dfs(adj,it,node,vis))return true;
+        }
+    }
+    return false;
+}
+
+bool cycleDetectionBFS(int v, vector<int> adj[])
+{
+    vector<int>vis(n+1,0);
+    for(int i=0;i<=n;i++){
+        if(!vis[i]){
+            if(dfs(adj,i,-1,vis))return true;
+        }
+    }
+
+    return false;
+}
