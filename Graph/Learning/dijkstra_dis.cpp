b0eb786805950144425d81706bdacd3312c82db6
Kritika Rag <kritikarag.kr@gmail.com>
2022-10-12 03:37:30 +0530

    modified:   Graph/Learning/dijkstra_dis.cpp

--- Graph/Learning/dijkstra_dis.cpp
+++ Graph/Learning/dijkstra_dis.cpp
@@ -2,26 +2,39 @@
 
 Dijksta's Algorithm : Shortest distance for undirected weighted graph with positive edge weights
 
-
+Using Queue:-
+TC: ElogV
 vector<int>dijkstra(int v, vector<pair<int,int>>adj[], int src){
     vector<int>dis(v+1,INT_MAX);
-    queue<pair<int,int>,vector<pair<int,int>,greater<pair<int,int>>>pq;
-    pq.push(src);
+    priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+    pq.push({0,src});
     dis[src] = 0;
 
-    while(!q.empty()){
-        int node = pq.top().first;
-        int weight = pq.top().second;
-
+    while(!pq.empty()){
+        int weight = pq.top().first;
+        int node = pq.top().second;
+        pq.pop();
         for(auto it: adj[node]){
-            int temp = it.first;
-            int curr = it.second;
+            int curr = it[1];
+            int temp = it[0];
 
             if(dis[temp]>weight+curr){
                 dis[temp] = weight+curr;
-                pq.push({temp, dis[temp]}); 
+                pq.push({dis[temp],temp}); 
             }
         }
     }
+    return dis;
+}
+
+Using Set : -
+
+vector<int>dijkstra(int v, vector<pair<int, int>> adj[], int src)
+{
+    vector<int> dis(v + 1, INT_MAX);
+    set<pair<int,int>>s;
+
+
+    
     return dis;
 }
