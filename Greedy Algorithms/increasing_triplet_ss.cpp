97496a169daca9c943a1ec0d41465a8cfea7aad4
Kritika Rag <kritikarag.kr@gmail.com>
2022-10-11 15:54:20 +0530

    new file:   Greedy Algorithms/increasing_triplet_ss.cpp

---
+++ Greedy Algorithms/increasing_triplet_ss.cpp
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/increasing-triplet-subsequence/
+
+bool increasingTriplet(vector<int> &nums)
+{
+    int one = INT_MAX, two = INT_MAX;
+    for (auto num : nums)
+    {
+        if (num < one)
+            one = num;
+        else if (one < num && num < two)
+            two = num;
+        else if (two < num)
+            return true;
+    }
+    return false;
+}
