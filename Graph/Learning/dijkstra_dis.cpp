16aaa634a76f4df27e1e9e1a9d4190065966fc96
Kritika Rag <kritikarag.kr@gmail.com>
2022-10-11 15:52:41 +0530

    modified:   Graph/Learning/dijkstra_dis.cpp

--- Graph/Learning/dijkstra_dis.cpp
+++ Graph/Learning/dijkstra_dis.cpp
@@ -1,18 +1,27 @@
-// Shortest distance for undirected weighted graph
+// https://practice.geeksforgeeks.org/problems/implementing-dijkstra-set-1-adjacency-matrix/1
+
+Dijksta's Algorithm : Shortest distance for undirected weighted graph with positive edge weights
+
 
 vector<int>dijkstra(int v, vector<pair<int,int>>adj[], int src){
     vector<int>dis(v+1,INT_MAX);
-    queue<pair<int,int>,vector<pair<int,int>,greater<pair<int,int>>pq;
-    q.push(src);
+    queue<pair<int,int>,vector<pair<int,int>,greater<pair<int,int>>>pq;
+    pq.push(src);
     dis[src] = 0;
 
     while(!q.empty()){
-        int node = q.front();
-        q.pop();
+        int node = pq.top().first;
+        int weight = pq.top().second;
+
         for(auto it: adj[node]){
-            if(dis[it.first]>dis[node]+it.second){
-                dis[it.first]=dis[node]+it.second;
+            int temp = it.first;
+            int curr = it.second;
+
+            if(dis[temp]>weight+curr){
+                dis[temp] = weight+curr;
+                pq.push({temp, dis[temp]}); 
             }
         }
     }
+    return dis;
 }
