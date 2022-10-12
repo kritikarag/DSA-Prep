25d60be8a7090c7a68e62878f8c9d15f724b9b88
Kritika Rag <kritikarag.kr@gmail.com>
2022-10-12 05:35:38 +0530

    modified:   Dynamic Programming/1-D DP/frog_jump_with_K_dist.cpp

--- Dynamic Programming/1-D DP/frog_jump_with_K_dist.cpp
+++ Dynamic Programming/1-D DP/frog_jump_with_K_dist.cpp
@@ -17,7 +17,7 @@ int frogJump(int n, vector<int>&heights, vector<int>&dp,int k){
     vector<int>dp(n,INT_MAX);
     if(dp[n]!=-1)return dp[n];
     for(int j=k;j>0;j--){
-        if(i>=j)dp[n] = min(dp[n],frogJump(n-j,heights,dp,k)+abs(heights[n]-heights[n-j]));
+        if(n>=j)dp[n] = min(dp[n],frogJump(n-j,heights,dp,k)+abs(heights[n]-heights[n-j]));
     }
 }
 
