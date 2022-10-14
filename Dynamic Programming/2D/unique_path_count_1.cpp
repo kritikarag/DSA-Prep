f33cab156a7839672498b583e521f39782ad3be2
Kritika Rag <kritikarag.kr@gmail.com>
2022-10-14 06:18:56 +0530

    modified:   Dynamic Programming/2D/unique_path_count_1.cpp

--- Dynamic Programming/2D/unique_path_count_1.cpp
+++ Dynamic Programming/2D/unique_path_count_1.cpp
@@ -10,6 +10,20 @@ int uniquePathSum(int i, int j){
     else return uniquePathSum(i,j-1)+uniquePathSum(i-1,j);
 }
 
+//Memorization
+
+int uniquePathSum(int i, int j, vector<vector<int>>&dp){
+    if(i<=0||j<=0)return dp[i][j]=0;
+    if(i==1||j==1)return dp[i][j]=1;
+
+    if(dp[i][j]!=-1)return dp[i][j];
+
+    int left = uniquePathSum(i-1,j,dp);
+    int up = uniquePathSum(i,j-1,dp);
+
+    return dp[i][j] = left + up;
+}
+
 //TABULATION
 
 int uniquePathSum(int n,int m){
@@ -27,19 +41,35 @@ int uniquePathSum(int n,int m){
 
 //SPACE OPTIMIZATION
 int uniquePathSum(int n, int m){
-    vector<int>prev(n+1,0);
+    vector<int>prev(m+1,0);
     for(int i=1;i<=n;i++){
         vector<int>curr(m+1,0);
         for(int j=1;j<=m;j++){
             if(i==1&&j==1)curr[j]=1;
             else curr[j]= curr[j-1]+prev[j];
         }
-
         prev = curr;
     }
 
     return prev[m];
 }
+
+//Short Logistic Approach
+
+int uniquePathSum(int n, int m){
+
+    int row = n + m -2;
+    int col = n -1;
+
+    double res =1;
+
+    for(int i=1;i<=col;i++){
+        res*=(row-col+i)/i;
+    }
+
+    return (int)res;
+}
+
 int main(){
     cout<<uniquePathSum(3,2)<<endl;
     return 0;
