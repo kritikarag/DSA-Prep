7ef5930fb00b571c05ab875fb00bf3c975d93415
Kritika Rag <kritikarag.kr@gmail.com>
2022-10-14 06:44:16 +0530

    modified:   Dynamic Programming/2D/maze_obstacles.cpp

--- Dynamic Programming/2D/maze_obstacles.cpp
+++ Dynamic Programming/2D/maze_obstacles.cpp
@@ -12,20 +12,41 @@ int mazeObstacles(int i, int j, vector<vector<int>>&vec){
     return mazeObstacles(i - 1, j, vec) + mazeObstacles(i, j - 1, vec);
 }
 
+//MEMORIZATION
+
+int mazeObstacles(int i, int j, vector<vector<int>> &vec, vector<vector<int>> &dp)
+{
+    if (i <= 0 || j <= 0 || vec[i - 1][j - 1] == -1)
+        return 0;
+    if (i == 1 && j == 1)
+        return dp[i][j] = 1;
+    if (dp[i][j] != -1)
+        return dp[i][j];
+
+    int left = mazeObstacles(i - 1, j, vec, dp);
+    int up = mazeObstacles(i, j - 1, vec, dp);
+
+    return dp[i][j] = left + up;
+}
+
 //TABULATION
 
 int mazeObstacles(int n, int m, vector<vector<int>> &vec)
 {
-    vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
 
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=m;j++){
-            if(i==1&&j==1)dp[i][j]=1;
-            else if(vec[i-1][j-1]==-1)dp[i][j]=0;
-            else dp[i][j] = dp[i-1][j] + dp[i][j-1];
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            if (i == 1 && j == 1)
+                dp[i][j] = 1;
+            else if (vec[i - 1][j - 1] == -1)
+                dp[i][j] = 0;
+            else
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
         }
     }
-
     return dp[n][m];
 }
 
@@ -33,7 +54,7 @@ int mazeObstacles(int n, int m, vector<vector<int>> &vec)
 
 int mazeObstacles(int n, int m, vector<vector<int>> &vec)
 {
-    vector<int>prev(n + 1,0);
+    vector<int>prev(m + 1,0);
 
     for (int i = 1; i <= n; i++)
     {
