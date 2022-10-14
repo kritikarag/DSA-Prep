abb3708300a061d096584f7ee7e16c433b2cc904
Kritika Rag <kritikarag.kr@gmail.com>
2022-10-14 11:34:27 +0530

    modified:   Dynamic Programming/DP On Subsequences/subset_sum.cpp

--- Dynamic Programming/DP On Subsequences/subset_sum.cpp
+++ Dynamic Programming/DP On Subsequences/subset_sum.cpp
@@ -29,23 +29,21 @@ bool subsetsum(int ind, int target, vector<int>&vec, vector<vector<int>>&dp){
 //TABULATION
 
 bool subsetsum(int target,vector<int>&vec){
-    vector<vector<bool>dp(n,vector<bool>(target+1,false));
+    vector<vector<bool>dp(n+1,vector<bool>(target+1,false));
 
-    for(int i=0;i<=target;i++)dp[0][i]=false;
-    for(int i=0;i<n;i++)dp[i][0]=true;
+    for(int i=0;i<=n;i++)dp[i][0]=true;
 
-    if(vec[0]<=target)dp[0][vec[0]]=true;
+    if(vec[0]<=target)dp[1][vec[0]]=true;
 
-    for(int i=1;i<n;i++){
+    for(int i=1;i<=n;i++){
         for(int j=1;j<=target;j++){
             //NOT TAKEN
             dp[i][j] =dp[i-1][j];
             //TAKEN
-            if(vec[i]<=j)dp[i][j]|=dp[i-1][j-vec[i]];
+            if(vec[i-1]<=j)dp[i][j]|=dp[i-1][j-vec[i-1]];
         }
     }
-
-    return dp[n-1][target];
+    return dp[n][target];
 }
 
 //SPACE OPTIMIZATION
