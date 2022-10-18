34222af37fbb06ad3a4b5f4b0763730396eee800
Kritika Rag <kritikarag.kr@gmail.com>
2022-10-19 03:49:04 +0530

    DP On String/delete_operations_for_two_strings.cpp

---
+++ Dynamic Programming/DP On String/delete_operations_for_two_strings.cpp
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/delete-operation-for-two-strings/
+
+int minDistance(string word1, string word2)
+{
+    int n = word1.length(), m = word2.length();
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            if (word1[i - 1] == word2[j - 1])
+            {
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+            }
+            else
+            {
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+
+    return m + n - 2 * dp[n][m];
+}
