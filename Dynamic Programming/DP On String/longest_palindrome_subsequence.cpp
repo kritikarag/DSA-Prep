17068754f5d093036f0868c1a66d6b843ce054d2
Kritika Rag <kritikarag.kr@gmail.com>
2022-10-19 03:05:28 +0530

    Dynamic Programming/DP On String/longest_palindrome_subsequence.cpp

--- Dynamic Programming/DP On String/longest_palindrome_subsequence.cpp
+++ Dynamic Programming/DP On String/longest_palindrome_subsequence.cpp
@@ -1,7 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-string lps(int n,int m, string &s1, string &s2){
+string lps(int n,int m, string &s1,){
+    string s2 = s1;
+    reverse(s2.begin(),s2.end());
     vector<vector<int>>dp(n+1,vector<int>(m+1,0));
 
     for (int ind1 = 1; ind1 <= n; ind1++)
@@ -11,7 +13,7 @@ string lps(int n,int m, string &s1, string &s2){
             if (s1[ind1 - 1] == s2[ind2 - 1])
                 dp[ind1][ind2] = 1 + dp[ind1 - 1][ind2 - 1];
             else
-                dp[ind1][ind2] = 0 + max(dp[ind1 - 1][ind2], dp[ind1][ind2 - 1]);
+                dp[ind1][ind2] = max(dp[ind1 - 1][ind2], dp[ind1][ind2 - 1]);
         }
     }
 
