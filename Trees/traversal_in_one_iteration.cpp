2443944b9306ee2490b236ae3934e61bced6df3f
Kritika Rag <kritikarag.kr@gmail.com>
2022-10-19 10:41:44 +0530

    modified:   Trees/traversal_in_one_iteration.cpp

--- Trees/traversal_in_one_iteration.cpp
+++ Trees/traversal_in_one_iteration.cpp
@@ -5,15 +5,24 @@ void traversal(TreeNode* root){
     st.push({root,1});
     while(!st.empty()){
         auto node = st.top();
+        st.pop();
         if(node.second==1){
             pre.push_back(node.first->val);
             node.second++;
             st.push(node);
+
+            if(node.first->left){
+                st.push({node.first->left,1});
+            }
         }
         else if(node.second==2){
             in.push_back(node.first->val);
             node.second++;
             st.push(node);
+            if (node.first->right)
+            {
+                st.push({node.first->right, 1});
+            }
         }
         else{
             pos.push_back(node.first->val);
