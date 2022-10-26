d69287f510291c4a0ba2da75521bd8903ff8e948
Kritika Rag <kritikarag.kr@gmail.com>
2022-10-26 08:03:09 +0530

    modified:   Hashing/continuous_subarray_sum_II.cpp

--- Hashing/continuous_subarray_sum_II.cpp
+++ Hashing/continuous_subarray_sum_II.cpp
@@ -4,13 +4,39 @@ bool checkSubarraySum(vector<int> &nums, int k)
 {
     map<int, int> mp;
     int sum = 0, pretemp = 0;
-    for (int i = 1; i < nums.size(); i++)
+    for (int i = 0; i < nums.size(); i++)
     {
         sum = (sum + nums[i]) % k; // [edited]
         if (mp[sum])
             return true;
         mp[pretemp]++;
         pretemp = sum;
+    }
+
+    return false;
+}
+
+Optimized Version :
+
+bool checkSubarraySum(vector<int> &nums, int k)
+{
+    int prefSum = 0;
+    unordered_map<int, int> mp;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        prefSum += nums[i];
+        prefSum %= k;
+
+        if (prefSum == 0 && i)
+            return true;
+
+        if (mp.find(prefSum) != mp.end())
+        {
+            if (i - mp[prefSum] > 1)
+                return true;
+        }
+        else
+            mp[prefSum] = i;
     }
 
     return false;
