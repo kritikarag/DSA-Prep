c78233cd38b36b852d57d379a7a312f80a882243
Kritika Rag <kritikarag.kr@gmail.com>
2022-10-27 11:38:16 +0530

    modified:   Matrix Problem/image_overlap.cpp

--- Matrix Problem/image_overlap.cpp
+++ Matrix Problem/image_overlap.cpp
@@ -27,3 +27,23 @@ int largestOverlap(vector<vector<int>> &img1, vector<vector<int>> &img2)
 
     return res;
 }
+
+int largestOverlap(vector<vector<int>> &img1, vector<vector<int>> &img2)
+{
+    vector<int> LA, LB;
+    int N = img1.size();
+    unordered_map<int, int> count;
+    for (int i = 0; i < N * N; ++i)
+        if (img1[i / N][i % N] == 1)
+            LA.push_back(i / N * 100 + i % N);
+    for (int i = 0; i < N * N; ++i)
+        if (img2[i / N][i % N] == 1)
+            LB.push_back(i / N * 100 + i % N);
+    for (int i : LA)
+        for (int j : LB)
+            count[i - j]++;
+    int res = 0;
+    for (auto it : count)
+        res = max(res, it.second);
+    return res;
+}
