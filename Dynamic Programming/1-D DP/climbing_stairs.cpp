99d55c2b3e7d012cabacf8812a287eb98a9f8627
Kritika Rag <kritikarag.kr@gmail.com>
2022-10-27 12:04:23 +0530

    modified:   Dynamic Programming/1-D DP/climbing_stairs.cpp

--- Dynamic Programming/1-D DP/climbing_stairs.cpp
+++ Dynamic Programming/1-D DP/climbing_stairs.cpp
@@ -10,7 +10,7 @@ int climbStairs(int n) {
         return res;
 }
 int climbStairs(int n){
-    if(n==0)return 0;
+    if(n==2)return 2;
     if(n==1)return 1;
 
     return climbStairs(n-1)+climbStairs(n-2);
