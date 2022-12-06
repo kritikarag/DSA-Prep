68bd9e47cbd17b7c24e0ea4bc9af15463fbe6904
Kritika Rag <kritikarag.kr@gmail.com>
2022-12-06 22:58:31 +0530

    new file:   Linked List/oddeven.cpp

---
+++ Linked List/oddeven.cpp
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/odd-even-linked-list/
+
+ListNode *odd = NULL, *headodd = NULL, *even = NULL, *headeven = NULL;
+
+if (!head || !head->next || !head->next->next)
+    return head;
+
+odd = head;
+headodd = odd;
+even = head->next;
+headeven = even;
+
+while (even && even->next)
+{
+    odd->next = odd->next->next;
+    even->next = even->next->next;
+    odd = odd->next;
+    even = even->next;
+}
+
+odd->next = headeven;
+return headodd;
