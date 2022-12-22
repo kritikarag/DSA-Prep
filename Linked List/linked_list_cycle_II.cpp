b92236e4de64e4eda0d27756cc1ed39f0d8e67d8
Kritika Rag <kritikarag.kr@gmail.com>
2022-12-22 20:19:47 +0530

    modified:   Linked List/linked_list_cycle_II.cpp

--- Linked List/linked_list_cycle_II.cpp
+++ Linked List/linked_list_cycle_II.cpp
@@ -1,8 +1,12 @@
 // https://leetcode.com/problems/linked-list-cycle-ii/description/
 
 /*An easier formula to explain to the interviewer:
+
+L1 is defined as the distance between the head point and entry point
+L2 is defined as the distance between the entry point and the meeting point
+x is defined as the remaining length of the cycle
 Distance traveled by slow when they meet: L1+L2
-Distance traveled by fast when they meet: L1+L2+x+L2, 
+Distance traveled by fast when they meet: L1+L2+x+L2,
 where x is the remaining length of the cycle (meeting point to start of the cycle).
 
 2(L1+L2) = L1+L2+x+L2
