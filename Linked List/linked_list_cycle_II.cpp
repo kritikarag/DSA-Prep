436e714b14e8b0199991fcf326917b7450ceb4d7
Kritika Rag <kritikarag.kr@gmail.com>
2022-12-22 20:18:22 +0530

    new file:   Linked List/linked_list_cycle_II.cpp

---
+++ Linked List/linked_list_cycle_II.cpp
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/linked-list-cycle-ii/description/
+
+/*An easier formula to explain to the interviewer:
+Distance traveled by slow when they meet: L1+L2
+Distance traveled by fast when they meet: L1+L2+x+L2, 
+where x is the remaining length of the cycle (meeting point to start of the cycle).
+
+2(L1+L2) = L1+L2+x+L2
+2L1 + 2L2 = L1+2L2+x
+=> x = L1
+
+so we need to move L1 steps from the current meeting point to reach the entry point of the cycle.*/
+
+ListNode *detectCycle(ListNode *head)
+{
+    ListNode *fast = head, *slow = head, *entry = head;
+    while (fast && fast->next)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast)
+        {
+            while (entry != slow)
+            {
+                entry = entry->next;
+                slow = slow->next;
+            }
+            return entry;
+        }
+    }
+    return NULL;
+}
