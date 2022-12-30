cc165575964d8dcb41c07cdf4796033f243a317a
Kritika Rag <kritikarag.kr@gmail.com>
2022-12-30 13:02:19 +0530

    modified:   Trees/array_to_bst.cpp

--- Trees/array_to_bst.cpp
+++ Trees/array_to_bst.cpp
@@ -1,15 +1,34 @@
 void solve(int i,int j,vector<int>&nums, vector<int>&ans){
-        if(i<=j){
-            int mid = i +(j-i)/2;
-            ans.push_back(nums[mid]);
-            solve(i,mid-1,nums,ans);
-            solve(mid+1,j,nums,ans);
-        }
+    if(i<=j){
+        int mid = i +(j-i)/2;
+        ans.push_back(nums[mid]);
+        solve(i,mid-1,nums,ans);
+        solve(mid+1,j,nums,ans);
     }
-    vector<int> sortedArrayToBST(vector<int>& nums) {
-        // Code here
-        int n=nums.size();
-        vector<int>ans;
-        solve(0,n-1,nums,ans);
-        return ans;
-    }
+}
+vector<int> sortedArrayToBST(vector<int>& nums) {
+    // Code here
+    int n=nums.size();
+    vector<int>ans;
+    solve(0,n-1,nums,ans);
+    return ans;
+}
+
+
+// LinkedList Approach
+
+TreeNode *solve(int i, int j, vector<int> &nums)
+{
+    if (i > j)
+        return NULL;
+    int mid = i + (j - i) / 2;
+    TreeNode *root = new TreeNode(nums[mid]);
+    root->left = solve(i, mid - 1, nums);
+    root->right = solve(mid + 1, j, nums);
+    return root;
+}
+TreeNode *sortedArrayToBST(vector<int> &nums)
+{
+    int n = nums.size();
+    return solve(0, n - 1, nums);
+}
