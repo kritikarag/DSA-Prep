6aa4bc5acbbae27d0b6c5b4fced3f5c4e63958a7
Kritika Rag <kritikarag.kr@gmail.com>
2023-01-02 05:28:26 +0530

    modified:   Trees/kth_smallest_element.cpp

--- Trees/kth_smallest_element.cpp
+++ Trees/kth_smallest_element.cpp
@@ -1,5 +1,21 @@
 // https://practice.geeksforgeeks.org/problems/find-k-th-smallest-element-in-bst/1
+// https://leetcode.com/problems/kth-smallest-element-in-a-bst/description/
+vector<int> ans;
+void inorder(TreeNode *root)
+{
+    if (!root)
+        return;
+    inorder(root->left);
+    ans.push_back(root->val);
+    inorder(root->right);
+}
+int kthSmallest(TreeNode *root, int k)
+{
+    inorder(root);
+    return ans[k - 1];
+}
 
+// Inorder SPace Optimized
 void solve(Node *root, int &K, int &ans)
 {
     if (!root)
