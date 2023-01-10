cb31ac3fca1cb5fc614b6dc75bd8db30751b320a
Kritika Rag <kritikarag.kr@gmail.com>
2023-01-11 03:59:33 +0530

    modified:   Dynamic Programming/Practice/max_product_subarray.cpp

--- Dynamic Programming/Practice/max_product_subarray.cpp
+++ Dynamic Programming/Practice/max_product_subarray.cpp
@@ -1,3 +1,27 @@
+//
+
+/*A way to understand the reason why the maximum subarray must be a prefix of suffix of the array:
+suppose there's no 0 in the array:
+
+if number of negative number is odd, and left most is i and right most is j,
+then we can only either keep A[:j] or A[i+1:] to make it max
+if it's even, then we just multiply all the numbers.
+0 will divide the array to several separated subarrays*/
+
+int maxProduct(vector<int> A)
+{
+    int n = A.size(), res = A[0], l = 0, r = 0;
+    for (int i = 0; i < n; i++)
+    {
+        l = (l ? l : 1) * A[i];
+        r = (r ? r : 1) * A[n - 1 - i];
+        res = max(res, max(l, r));
+    }
+    return res;
+}
+
+
+
 int maxProduct(vector<int>& nums) {
         int res=nums[0];
         int mini=nums[0],maxi=nums[0];
@@ -12,3 +36,5 @@ int maxProduct(vector<int>& nums) {
         
         return res;
     }
+
+
