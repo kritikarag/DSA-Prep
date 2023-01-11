814ccf86120bd1131b0120c4a03e2cdc20abf151
Kritika Rag <kritikarag.kr@gmail.com>
2023-01-11 19:03:08 +0530

    Sorting And Searching/nums_pair_satisfying_inequality.cpp

---
+++ Sorting And Searching/nums_pair_satisfying_inequality.cpp
@@ -0,0 +1,42 @@
+// https://leetcode.com/problems/number-of-pairs-satisfying-inequality/description/
+
+long long int count = 0;
+void checkcount(vector<int> &vec, int start, int mid, int end, int d)
+{
+    int l = start, r = mid + 1;
+    while (l <= mid && r <= end)
+    {
+        if (vec[l] <= vec[r] + d)
+        {
+            count += (end - r + 1);
+            l++;
+        }
+        else
+        {
+            r++;
+        }
+    }
+    sort(vec.begin() + start, vec.begin() + end + 1);
+    return;
+}
+void mergesort(vector<int> &vec, int start, int end, int d)
+{
+    if (start == end)
+        return;
+    int mid = (start + end) / 2;
+    mergesort(vec, start, mid, d);
+    mergesort(vec, mid + 1, end, d);
+
+    checkcount(vec, start, mid, end, d);
+    return;
+}
+long long numberOfPairs(vector<int> &nums1, vector<int> &nums2, int d)
+{
+    int n = nums1.size();
+    vector<int> c(n);
+    for (int i = 0; i < n; i++)
+        c[i] = nums1[i] - nums2[i];
+    mergesort(c, 0, n - 1, d);
+
+    return count;
+}
