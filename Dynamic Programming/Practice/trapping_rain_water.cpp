91652228d3a3082718812924f21b4bc723ff79b5
Kritika Rag <kritikarag.kr@gmail.com>
2023-01-15 17:47:41 +0530

    Dynamic Programming/Practice/trapping_rain_water.cpp

---
+++ Dynamic Programming/Practice/trapping_rain_water.cpp
@@ -0,0 +1,46 @@
+//
+
+int trap(vector<int> &heights)
+{
+    int n = heights.size();
+    int left = 0;
+    int right = n - 1;
+    int res = 0;
+    int maxleft = 0, maxright = 0;
+    while (left <= right)
+    {
+        if (heights[left] <= heights[right])
+        {
+            if (heights[left] >= maxleft)
+                maxleft = heights[left];
+            else
+                res += maxleft - heights[left];
+            left++;
+        }
+        else
+        {
+            if (heights[right] >= maxright)
+                maxright = heights[right];
+            else
+                res += maxright - heights[right];
+            right--;
+        }
+    }
+    return res;
+}
+
+int trap(vector<int> &heights)
+{
+    int n = heights.size();
+    int left = 0;
+    int right = n - 1;
+    int res = 0;
+    int currlevel = 0, maxlevel = 0;
+    while (left <= right)
+    {
+        currlevel = heights[heights[left] < heights[right] ? left++ : right--];
+        maxlevel = max(currlevel, maxlevel);
+        res += maxlevel - currlevel;
+    }
+    return res;
+}
