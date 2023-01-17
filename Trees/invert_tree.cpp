a2068224eda54f4258d72fa9c64c5a5e7cc705ae
Kritika Rag <kritikarag.kr@gmail.com>
2023-01-17 07:43:12 +0530

    modified:   Trees/invert_tree.cpp

--- Trees/invert_tree.cpp
+++ Trees/invert_tree.cpp
@@ -1,9 +1,29 @@
+// https://leetcode.com/problems/invert-binary-tree/description/
+
+Recursive Approach:
+
 TreeNode* invertTree(TreeNode* root) {
-        if(root==NULL)return NULL;
-        
-        TreeNode* temp = invertTree(root->left);
-        root->left = invertTree(root->right);
-        root->right=temp;
-        
-        return root;
+    if(root==NULL)return NULL;
+    
+    TreeNode* temp = invertTree(root->left);
+    root->left = invertTree(root->right);
+    root->right=temp;
+    
+    return root;
+}
+
+Iterative Approach:
+
+TreeNode* invertTree(TreeNode* root) {
+    if(!root)return NULL;
+    stack<TreeNode*>st;
+    st.push(root);
+    while(!st.empty()){
+        auto temp = st.top();
+        st.pop();
+        if(temp->left)st.push(temp->left);
+        if(temp->right)st.push(temp->right);
+        swap(temp->left,temp->right);
     }
+    return root;
+}
