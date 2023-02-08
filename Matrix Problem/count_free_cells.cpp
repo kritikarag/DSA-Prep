fd6e422624624475592d9141d310615f6f7d3cf9
Kritika Rag <kritikarag.kr@gmail.com>
2023-02-08 22:36:35 +0530

    new file:   Matrix Problem/count_free_cells.cpp

---
+++ Matrix Problem/count_free_cells.cpp
@@ -0,0 +1,28 @@
+// https://practice.geeksforgeeks.org/problems/90a81c305b1fe939b9230a67824749ceaa493eab/1
+
+vector<long long int> countZero(int n, int k, vector<vector<int>> &arr)
+{
+    // Code Here
+    vector<long long int> ans;
+    unordered_map<int, int> r, c;
+    int rowc = 0;
+    int colc = 0;
+    for (int i = 0; i < k; ++i)
+    {
+        int x = arr[i][0];
+        int y = arr[i][1];
+        if (r.find(x) == r.end())
+        {
+            ++r[x];
+            ++rowc;
+        }
+        if (c.find(y) == c.end())
+        {
+            ++c[y];
+            ++colc;
+        }
+        long long int a = n * n - ((rowc + colc) * n) + (rowc * colc);
+        ans.push_back(a);
+    }
+    return ans;
+}
