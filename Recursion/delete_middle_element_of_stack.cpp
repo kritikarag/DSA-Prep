e2218e3c796f357962a6401449d27e7ec9dc6951
Kritika Rag <kritikarag.kr@gmail.com>
2023-02-14 14:29:51 +0530

    new file:   Recursion/delete_middle_element_of_stack.cpp

---
+++ Recursion/delete_middle_element_of_stack.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void deletemiddle(stack<int>&st, int count){
+    if(count>=st.size()-1){
+        st.pop();
+        return;
+    }
+    int temp = st.top();
+    st.pop();
+    deletemiddle(st,count+1);
+    st.push(temp);
+    return;
+}
+
+int main()
+{
+    stack<int> st;
+
+    st.push(42);
+    st.push(16);
+    st.push(76);
+    st.push(53);
+    st.push(34);
+
+    int n = st.size();
+    deletemiddle(st,0);
+
+    for(int i=0;i<n-1;i++){
+        cout<<st.top()<<" ";
+        st.pop();
+    }
+    cout<<endl;
+
+    return 0;
+}
