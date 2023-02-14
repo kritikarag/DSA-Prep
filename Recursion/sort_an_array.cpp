d000b8367d33d1c2b79d6afeeebdf01f701d4306
Kritika Rag <kritikarag.kr@gmail.com>
2023-02-14 10:39:22 +0530

    modified:   Recursion/sort_an_array.cpp

--- Recursion/sort_an_array.cpp
+++ Recursion/sort_an_array.cpp
@@ -1,3 +1,41 @@
-void sort(vector<int>&vec){
-    
+#include<bits/stdc++.h>
+using namespace std;
+
+void recursiveinsert(vector<int>&vec, int x){
+    if (vec.size() == 0 || vec[vec.size() - 1] <= x)
+    {
+        vec.push_back(x);
+        return;
+    }
+    int val = vec[vec.size()-1];
+    vec.pop_back();
+    recursiveinsert(vec,x);
+    vec.push_back(val);
+    return;
+}
+
+void recursivesort(vector<int>&vec){
+    if(vec.size()<=1)return;
+
+    int temp = vec[vec.size()-1];
+    vec.pop_back();
+
+    recursivesort(vec);
+    recursiveinsert(vec, temp);
+}
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int>vec(n);
+    for(int i=0;i<n;i++)cin>>vec[i];
+
+    recursivesort(vec);
+
+    for(int i=0;i<n;i++){
+        cout<<vec[i]<<" ";
+    }
+    cout<<endl;
+
+    return 0;
 }
