9c1b24ff2b4e04820394ca28aa6ba586633fdfba
Kritika Rag <kritikarag.kr@gmail.com>
2023-02-16 20:54:52 +0530

    modified:   Recursion/reverse_a_stack.cpp

--- Recursion/reverse_a_stack.cpp
+++ Recursion/reverse_a_stack.cpp
@@ -1,6 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void recurrent_reverse(stack<int>&st){
+
+}
+
 int main(){
-    
+    int n;
+    cin>>n;
+    stack<int>st;
+    for(int i=0;i<n;i++){
+        int x;
+        cin>>x;
+        st.push(x);
+    }
+
+    recurrent_reverse(st);
+
+    for(int i=0;i<n;i++){
+        cout<<st.top()<<" ";
+        st.pop();
+    }
+    cout<<endl;
+    return 0;
 }
