d3222601a1250b5862cd38f1029c0ee88b028fb4
Kritika Rag <kritikarag.kr@gmail.com>
2023-02-18 12:29:47 +0530

    modified:   Recursion/reverse_a_stack.cpp

--- Recursion/reverse_a_stack.cpp
+++ Recursion/reverse_a_stack.cpp
@@ -1,7 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void insert_at_the_bottom(stack<int>&st, int x){
+    if(st.empty()){
+        st.push(x);
+        return;
+    }
+
+    int temp = st.top();
+    st.pop();
+    insert_at_the_bottom(st,x);
+
+    st.push(temp);
+    
+}
+
 void recurrent_reverse(stack<int>&st){
+    if(st.size()<1)return;
+    int temp = st.top();
+    st.pop();
+    recurrent_reverse(st);
+
+    insert_at_the_bottom(st,temp);
 
 }
 
