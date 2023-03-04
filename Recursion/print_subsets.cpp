f9f44730ea37176e630b1406c69469ac677f24e5
Kritika Rag <kritikarag.kr@gmail.com>
2023-03-04 23:02:06 +0530

    modified:   Recursion/print_subsets.cpp

--- Recursion/print_subsets.cpp
+++ Recursion/print_subsets.cpp
@@ -1,9 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// To store only the unique elements
+unordered_set<string>s;
 void printsubsets(string str, int i, string res){
     if(i>=str.length()){
-        cout << res << " ";
+        //cout << res << " ";
+        s.insert(res);
         return;
     }
 
@@ -16,5 +19,8 @@ int main(){
     string str,res="";
     cin>>str;
     printsubsets(str,0,res);
+    for(auto it:s){
+        cout<<it<<endl;
+    }
     return 0;
 }
