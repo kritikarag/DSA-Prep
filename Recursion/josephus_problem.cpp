2eceebdd1087cc5861b07cec70c5601f8056c8bb
Kritika Rag <kritikarag.kr@gmail.com>
2023-03-17 15:17:20 +0530

    modified:   Recursion/josephus_problem.cpp

--- Recursion/josephus_problem.cpp
+++ Recursion/josephus_problem.cpp
@@ -1,7 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void calculate(vector<int>&vec, int i, int k){
+    if(vec.size()==1)return;
+    int index = (i+k)%vec.size();
+    vec.erase(vec.begin()+index);
+    calculate(vec,index,k);
+}
 int main(){
-    
+    int n,k;
+    cin>>n>>k;
+    k--;
+    vector<int>vec(n);
+    iota(vec.begin(),vec.end(),1);
+    calculate(vec,0,k);
+    cout<<vec[0]<<endl;
     return 0;
 }
