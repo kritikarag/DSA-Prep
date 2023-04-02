802eb52167c6a79bca2f5df74224eb02cbe62379
Kritika Rag <kritikarag.kr@gmail.com>
2023-04-02 20:49:16 +0530

    Binary Search/successful_pairs_of_spell_and_portions.cpp

---
+++ Binary Search/successful_pairs_of_spell_and_portions.cpp
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/successful-pairs-of-spells-and-potions/description/
+
+vector<int> successfulPairs(vector<int> &spells, vector<int> &potions, long long success)
+{
+    int postfix[100001] = {0};
+    for (auto potion : potions)
+        postfix[potion]++;
+    for (int i = 99999; i >= 0; --i)
+        postfix[i] += postfix[i + 1];
+
+    // No need extra space at all for storing final result
+    for (int i = 0; i < spells.size(); ++i)
+    {
+        long long val = success / (long long)spells[i];
+        if (success % (long long)spells[i] != 0)
+            val++;
+
+        spells[i] = val <= 1e5 ? postfix[val] : 0;
+    }
+    return spells;
+}
