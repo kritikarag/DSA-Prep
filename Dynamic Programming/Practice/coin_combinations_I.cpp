54da72536fafd9c7b11c27580e3fe23bb0b54e08
Kritika Rag <kritikarag.kr@gmail.com>
2023-04-11 21:11:27 +0530

    Dynamic Programming/Practice/coin_combinations_I.cpp

---
+++ Dynamic Programming/Practice/coin_combinations_I.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n, x;
+    cin >> n >> x;
+    vector<int>coins(n);
+    vector<int>dp(x+1);
+    int mod = 1e9+7;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> coins[i];
+    }
+    dp[0] = 1;
+    for (int weight = 0; weight <= x; weight++)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            if (weight - coins[i - 1] >= 0)
+            {
+                dp[weight] += dp[weight - coins[i - 1]];
+                dp[weight] %= mod;
+            }
+        }
+    }
+    cout << dp[x] << '\n';
+    return 0;
+}
