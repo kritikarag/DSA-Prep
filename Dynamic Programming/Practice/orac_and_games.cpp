5a381c981b520ab23acb3844dffde1a8a6159710
Kritika Rag <kritikarag.kr@gmail.com>
2023-04-23 18:06:05 +0530

    modified:   Dynamic Programming/Practice/orac_and_games.cpp

--- Dynamic Programming/Practice/orac_and_games.cpp
+++ Dynamic Programming/Practice/orac_and_games.cpp
@@ -10,36 +10,24 @@ int main(){
     while(t--){
         int n;
         cin >> n;
-        vector<int> costs(n);
+        vector<int> costs(n+1);
         
-        for (int i = 0; i < n; i++)
+        for (int i = 1; i <= n; i++)
         {
             cin >> costs[i];
         }
 
-        vector<vector<int>> dp(n, vector<int>(n, 0));
+        vector<int> dp(n+1, 1);  
 
-        dp[0][0] = 1;
-        for (int i = 1; i < n; i++)
+        for (int i = 1; i <= n; i++)
         {
-            if (costs[i] % costs[0] == 0)
-                dp[0][i] = 2;
-        }
-
-        for (int i = 1; i < n; i++)
-        {
-            int temp = costs[i];
-            dp[i][i] = max(dp[i - 1][i], 1);
-            int val = dp[i][i];
-            for (int j = i + 1; j < n; j++)
+            for (int j = 2*i; j <= n; j+=i)
             {
-                dp[i][j] = dp[i - 1][j];
-                if (costs[j] % temp == 0)
-                    dp[i][j] = max(dp[i][j], val + 1);
+                if(costs[j]>costs[i])dp[j] = max(dp[j],dp[i]+1);
             }
         }
 
-        cout << dp[n - 1][n - 1] << endl;
+        cout <<*max_element(dp.begin(),dp.end()) << endl;
     }
 
     return 0;
