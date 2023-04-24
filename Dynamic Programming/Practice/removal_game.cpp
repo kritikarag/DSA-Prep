9e30ec2cc77b770f796c54b89965cc4997d7f26c
Kritika Rag <kritikarag.kr@gmail.com>
2023-04-24 17:57:32 +0530

    modified:   Dynamic Programming/Practice/removal_game.cpp

--- Dynamic Programming/Practice/removal_game.cpp
+++ Dynamic Programming/Practice/removal_game.cpp
@@ -3,15 +3,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-//recursion
+//memorization
 
-// long long calculate(int i, int j, vector<long long> &scores, vector<vector<long long>> &dp, vector<vector<long long>> &sum)
-// {
-//     if(i==j)return scores[i];
-//     if(dp[i][j]!=LLONG_MAX)return dp[i][j];
+long long calculate(int i, int j, vector<long long> &scores, vector<vector<long long>> &dp, vector<vector<long long>> &sum)
+{
+    if(i==j)return scores[i];
+    if(dp[i][j]!=LLONG_MAX)return dp[i][j];
 
-//     return dp[i][j] = max(scores[i]+ sum[i+1][j] - calculate(i+1,j,scores,dp,sum), scores[j]+sum[i][j-1]-calculate(i,j-1,scores,dp,sum));
-// }
+    return dp[i][j] = max(scores[i]+ sum[i+1][j] - calculate(i+1,j,scores,dp,sum), scores[j]+sum[i][j-1]-calculate(i,j-1,scores,dp,sum));
+}
 
 int main()
 {
