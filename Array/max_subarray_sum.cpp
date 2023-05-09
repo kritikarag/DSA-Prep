e04bc7f894b58a8a068b1aa6074c8f3c11c4fe25
Kritika Rag <kritikarag.kr@gmail.com>
2023-05-09 21:50:11 +0530

    modified:   Array/max_subarray_sum.cpp

--- Array/max_subarray_sum.cpp
+++ Array/max_subarray_sum.cpp
@@ -12,16 +12,17 @@ int maxSubArray(vector<int> &nums)
     return res;
 }
 
-
-int maxSubArray(vector<int>& nums) {
-        int res=INT_MIN,curr=0;
-        for(int i=0;i<nums.size();i++){
-              curr+=nums[i];
-            res=max(res,curr);
-            if(curr<0){
-                curr=0;
-            }
+int maxSubArray(vector<int> &nums)
+{
+    int res = INT_MIN, curr = 0;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        curr += nums[i];
+        res = max(res, curr);
+        if (curr < 0)
+        {
+            curr = 0;
         }
-        return res;
     }
-};
+    return res;
+}
