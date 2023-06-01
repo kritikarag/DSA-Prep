4cae4d5f5629dbcd38638373296699a625b5624b
Kritika Rag <kritikarag.kr@gmail.com>
2023-06-01 23:08:50 +0530

    new file:   Trees/Practice/kth_ancestor.cpp

--- Trees/Practice/kefa_and_park.cpp
+++ Trees/Practice/kefa_and_park.cpp
@@ -1,27 +1,28 @@
-// 
+// https://codeforces.com/problemset/problem/580/C
 
 #include<bits/stdc++.h>
 #include<iostream>
 using namespace std;
 
-void solve(int node, int &res, int &curr, int m, vector<int>adj[], vector<bool>&vis,vector<int>&cats){
-    if(cats[node-1]==1)curr++;
-    else curr = 0;
+void solve(int node, int &res, int curr, int m, vector<int>adj[],vector<int>&cats){
 
+    
+    if(cats[node]==1)curr++;
+    else curr = 0;
+    if(curr > m) return;
     if(adj[node].size()==0 && curr<=m){
-        res++;
+        res++; 
         return;
     }
     for(int i:adj[node]){
-        solve(i, res, curr, m, adj, vis, cats);
+        solve(i, res, curr, m, adj, cats);
     }
 }
 
 int main(){
     int n,m;
     cin>>n>>m;
-    vector<int>adj[n];
-    vector<bool>vis(n+1,false);
+    vector<int>adj[n+1]; 
     vector<int>cats(n+1);
     cats[0] = 0;
     for(int i=1;i<=n;i++){
@@ -33,8 +34,9 @@ int main(){
         adj[x].push_back(y); 
     }
 
-    int res = 0,curr=0;
-    vis[0] = true;
-    solve(1,res,curr,m,adj,vis,cats);
+    int res = 0, curr = 0;
+    solve(1,res,curr,m,adj,cats);
     cout<<res<<endl;
+
+    return 0;
 }
---
+++ Trees/Practice/kth_ancestor.cpp
@@ -0,0 +1,55 @@
+// https://leetcode.com/problems/kth-ancestor-of-a-tree-node/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void getparents(int node, int parent, vector<int>tree[], vector<int>&parents){
+    parents[node] = parent;
+    for(int i:tree[node]){
+        if(i==parent)continue;
+        getparents(i,node,tree,parents);
+    }
+}
+
+int KthAncestor(int node, vector<int>tree[], int n, int k){
+
+    vector<int>parents(n);
+    getparents(1,0,tree,parents); 
+    while(k--){
+        node = parents[node];
+    }
+
+    return node;
+}
+
+
+// For a range of queries
+int n;
+vector<vector<int>>parents(n+1,vector<int>(17,0));
+int KthAncestor(int node, int parent, vector<int>tree[], int n){
+    parents[node][0] = parent;
+
+    for(int i=1;i<=16;i++){
+        parents[node][i] = parents[parents[node][i-1]][i-1];
+    } 
+
+    for(int it:tree[node]){
+        if(it==parent)continue;
+        KthAncestor(it,node,tree,n);
+    }
+}
+
+int KthAncestor(int node, vector<int> tree[], int n, int k)
+{
+
+    int count = 0,node;
+    while(k){
+        if(k&1){
+            node = parents[node][count];
+        }
+
+        k>>=1;
+        count++;
+    }
+    return node;
+}
