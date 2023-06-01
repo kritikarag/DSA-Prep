1713ebada19aa2bb54b7b168dc311e8f79658fa9
Kritika Rag <kritikarag.kr@gmail.com>
2023-06-02 04:51:28 +0530

    new file:   Trees/Practice/min_opr_to_convert_str1_to_str2.cpp

---
+++ Trees/Practice/min_opr_to_convert_str1_to_str2.cpp
@@ -0,0 +1,26 @@
+//
+
+int canYouMake(string &str, string &ptr)
+{
+    // Write your code here.
+    int n = str.length(), m = ptr.length();
+
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            if (str[i - 1] == ptr[j - 1])
+            {
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+            }
+            else
+            {
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+
+    return m + n - 2 * dp[n][m];
+}
