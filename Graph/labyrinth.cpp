1d0ed23e5671f1a0a7dcdedd86be4c6b8761508e
Kritika Rag <kritikarag.kr@gmail.com>
2023-06-13 21:41:52 +0530

    modified:   Graph/labyrinth.cpp

--- Graph/labyrinth.cpp
+++ Graph/labyrinth.cpp
@@ -1,33 +1,32 @@
 // https://cses.fi/problemset/task/1193
 #include<bits/stdc++.h>
 using namespace std;
-bool valid(int i,int j, int n, int m, vector<vector<char>>matrix){
-    if(i<0||j<0||i>=n||j>=m||matrix[i][j]=='#')return false;
+int dir[5] = {0, 1, 0, -1, 0};
+char d[4] = {'R','D','L','U'};
+vector<char>ans = {};
+bool valid(int i,int j,vector<vector<char>>matrix){
+    if(i<0||j<0||i>=matrix.size()||j>=matrix[0].size()||matrix[i][j]=='#')return false;
     return true;
 }
-bool bfs(int i, int j, vector<vector<char>>matrix,vector<vector<bool>>vis){
-    queue<pair<int,int>>q;
-    int n = matrix.size(), m = matrix[0].size();
-    int dir[5] = {0,1,0,-1,0};
+void dfs(int i, int j, vector<vector<char>>matrix,vector<vector<bool>>vis,vector<char>temp){
     vis[i][j] = true;
-    q.push({i,j});
-    while(!q.empty()){
-        int x = q.front().first;
-        int y = q.front().second;
-        q.pop();
-        if(matrix[i][j]=='B')return true;
-        for(int k=0;k<4;k++){
-            int row = x + dir[k];
-            int col = y + dir[k+1];
-            if(valid(row,col,n,m,matrix)&&!vis[row][col]){
-                q.push({row,col});
-            }
-        }                
+    if (matrix[i][j] == 'B' && (ans.size() == 0 || ans.size() > temp.size()))
+    {
+        ans = temp;
     }
-    return false;
+
+    for(int i=0;i<4;i++){
+        int row = i+dir[i];
+        int col = j+dir[i+1];
+        if(!vis[i][j] && valid(row,col,matrix)){
+            temp.push_back(d[i]);
+        }
+    }
+    temp.pop_back(); 
 }
 int main(){
-    int n,m;cin>>n>>m;
+    int n,m;
+    cin>>n>>m;
     vector<vector<char>>matrix(n,vector<char>(m));
     vector<vector<bool>>vis(n, vector<bool>(m,false));
     for(int i=0;i<n;i++){
@@ -38,7 +37,19 @@ int main(){
 
     for(int i=0;i<n;i++){
         for(int j=0;j<m;j++){
-            if(matrix[i][j]=='A')bfs(i,j,matrix,vis);
+            vector<char>temp;
+            if(matrix[i][j]=='A')dfs(i,j,matrix,vis,temp);
+        }
+    }
+
+    if(ans.size()==0){
+        cout<<"NO"<<endl;
+    }
+    else{
+        cout<<"YES"<<endl;
+        cout<<ans.size()<<endl;
+        for(int i =0;i<ans.size();i++){
+            cout<<ans[i];
         }
     }
     return 0;
