f6127885694eed5946306c2a83f6f05acf38e954
Kritika Rag <kritikarag.kr@gmail.com>
2023-09-05 21:35:38 +0530

    modified:   Binary Search/find_right_interval.cpp

--- Binary Search/find_right_interval.cpp
+++ Binary Search/find_right_interval.cpp
@@ -17,3 +17,58 @@ vector<int> findRightInterval(vector<vector<int>> &intvs)
 
     return ans;
 }
+
+vector<string> ans;
+unordered_map<string, pair<string, string>> mp;
+unordered_map<string, int> tt;
+unordered_map<string, int> spent;
+for (auto it : queries)
+{
+    if (it.size() == 4)
+    {
+        if (mp.find(it[1]) != mp.end())
+        {
+            ans.push_back("false");
+        }
+        else
+        {
+            mp[it[1]] = {it[2], it[3]};
+            ans.push_back("true");
+            spent[it[1]] = 0;
+        }
+    }
+    else if (it.size() == 3)
+    {
+        if (mp.find(it[1]) == mp.end())
+        {
+            ans.push_back("invalid_request");
+        }
+        else
+        {
+            if (tt.find(it[1]) == tt.end())
+            {
+                tt[it[1]] = stoi(it[2]);
+            }
+            else
+            {
+                int curr = stoi(it[2]) - tt[it[1]];
+                spent[it[1]] = spent[it[1]] + curr;
+                tt.erase(it[1]);
+            }
+            ans.push_back("registered");
+        }
+    }
+    else
+    {
+        if (mp.find(it[1]) == mp.end())
+        {
+            ans.push_back("");
+        }
+        else
+        {
+            ans.push_back(to_string(spent[it[1]]));
+        }
+    }
+}
+
+return ans;
