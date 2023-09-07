eaf0033cd5765ccf4114cc27e9271eeda04983eb
Kritika Rag <kritikarag.kr@gmail.com>
2023-09-07 23:22:36 +0530

    modified:   Graph/min_multiplication_to_reach_end.cpp

--- Graph/min_multiplication_to_reach_end.cpp
+++ Graph/min_multiplication_to_reach_end.cpp
@@ -29,3 +29,40 @@ int minimumMultiplications(vector<int> &arr, int start, int end)
     }
     return -1;
 }
+
+int minimumMultiplications(vector<int> &arr, int start, int end)
+{
+    // code here
+
+    if (start == end)
+        return 0;
+    int mod = 100000;
+
+    queue<pair<int, int>> q;
+
+    vector<bool> vis(mod, false);
+    q.push({start, 0});
+    vis[start] = true;
+
+    while (!q.empty())
+    {
+        int product = q.front().first;
+        int steps = q.front().second;
+        q.pop();
+
+        for (int i = 0; i < arr.size(); i++)
+        {
+            int curr = (product * arr[i]) % mod;
+            if (curr == end)
+                return steps + 1;
+
+            if (!vis[curr])
+            {
+                vis[curr] = true;
+                q.push({curr, steps + 1});
+            }
+        }
+    }
+
+    return -1;
+}
