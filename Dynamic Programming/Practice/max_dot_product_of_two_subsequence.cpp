e84b269db4866639e683156f14ebb7c201bd83bf
Kritika Rag <kritikarag.kr@gmail.com>
2023-10-08 21:59:12 +0530

    Added tabulation method

--- Dynamic Programming/Practice/max_dot_product_of_two_subsequence.cpp
+++ Dynamic Programming/Practice/max_dot_product_of_two_subsequence.cpp
@@ -30,3 +30,24 @@ int maxDotProduct(vector<int> &nums1, vector<int> &nums2)
 
 //Tabulation
 
+int maxDotProduct(vector<int> &nums1, vector<int> &nums2)
+{
+    int n = nums1.size();
+    int m = nums2.size();
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1e9));
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            int not_take_i = dp[i - 1][j];
+            int not_take_j = dp[i][j - 1];
+
+            int take = nums1[i - 1] * nums2[j - 1];
+            take = max(take, take + dp[i - 1][j - 1]);
+
+            dp[i][j] = max({not_take_i, not_take_j, take});
+        }
+    }
+    return dp[n][m];
+}
