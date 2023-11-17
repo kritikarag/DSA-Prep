2b295b9e521e81603142ef75fb21e6029ad7016b
Kritika Rag <kritikarag.kr@gmail.com>
2023-11-17 23:59:57 +0530

    Dynamic Programming/DP On String/count_palindromic_substring.cpp

---
+++ Dynamic Programming/DP On String/count_palindromic_substring.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// MEMOIZATION
+string str;
+vector<vector<int>> dp;
+int solve(int l, int r)
+{
+    if (l > r)
+        return 1;
+    if (l == r)
+        return 1;
+    if (dp[l][r] != -1)
+        return dp[l][r];
+    if (str[l] != str[r])
+        return dp[l][r] = 0;
+    if (solve(l + 1, r - 1))
+        return dp[l][r] = 1;
+
+    return dp[l][r] = 0;
+}
+int countSubstrings(string s)
+{
+    int n = s.size(), res = 0;
+    str = s;
+    dp = vector<vector<int>>(n, vector<int>(n, -1));
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i; j < n; j++)
+        {
+            res += solve(i, j);
+        }
+    }
+    return res;
+}
+
+
+// TABULATION
