54160c843d8715a792d4adfdf1177cd1aa339c4c
Kritika Rag <kritikarag.kr@gmail.com>
2023-12-05 23:33:00 +0530

    Dynamic Programming/Practice/longest_inc_subseq.cpp

--- Dynamic Programming/Practice/longest_inc_subseq.cpp
+++ Dynamic Programming/Practice/longest_inc_subseq.cpp
@@ -1,6 +1,34 @@
 // https://leetcode.com/problems/longest-increasing-subsequence/
 
 #include<bits/stdc++.h>
+using namespace std;
+
+// MEMOIZATION
+vector<vector<int>> dp;
+int n;
+int solve(int prev, int curr, vector<int> &nums)
+{
+    if (curr >= n)
+        return 0;
+    if (dp[prev + 1][curr] != -1)
+        return dp[prev + 1][curr];
+
+    int take = INT_MIN;
+    if (prev == -1 || nums[prev] < nums[curr])
+    {
+        take = 1 + solve(curr, curr + 1, nums);
+    }
+    int not_take = solve(prev, curr + 1, nums);
+    return dp[prev + 1][curr] = max(take, not_take);
+}
+int lengthOfLIS(vector<int> &nums)
+{
+    n = nums.size();
+    dp.resize(n, vector<int>(n, -1));
+    return solve(-1, 0, nums);
+}
+
+//TABULATION
 int lengthOfLIS(vector<int>& nums) {
         vector<vector<int>> dp(nums.size()+1, vector<int> (nums.size()+1, 0));
         
