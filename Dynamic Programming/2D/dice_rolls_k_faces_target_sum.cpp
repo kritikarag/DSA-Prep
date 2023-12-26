fe0e66433700e304f0e23c28c874f5fb12017666
Kritika Rag <kritikarag.kr@gmail.com>
2023-12-26 21:43:15 +0530

    Dynamic Programming/2D/dice_rolls_k_faces_target_sum.cpp

--- Dynamic Programming/2D/dice_rolls_k_faces_target_sum.cpp
+++ Dynamic Programming/2D/dice_rolls_k_faces_target_sum.cpp
@@ -1,4 +1,33 @@
 // https://leetcode.com/problems/number-of-dice-rolls-with-target-sum/
+#include<bits/stdc++.h>
+using namespace std;
+int mod = 1e9 + 7;
+vector<vector<int>> dp;
+int solve(int n, int target, int k)
+{
+    if (n == 0 && target == 0)
+        return dp[n][target] = 1;
+    if (n < 0)
+        return 0;
+    if (dp[n][target] != -1)
+        return dp[n][target];
+    dp[n][target] = 0;
+    for (int i = 1; i <= k; i++)
+    {
+        if (i <= target)
+        {
+            dp[n][target] += (solve(n - 1, target - i, k)) % mod;
+            dp[n][target] %= mod;
+        }
+    }
+    return dp[n][target] % mod;
+}
+int numRollsToTarget(int n, int k, int target)
+{
+    dp.resize(n + 1, vector<int>(target + 1, -1));
+
+    return solve(n, target, k);
+}
 
 int numRollsToTarget(int d, int f, int target)
 {
