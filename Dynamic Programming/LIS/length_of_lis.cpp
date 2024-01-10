074bca7f91eb13e1f474f410b72922af3bfe572c
Kritika Rag <kritikarag.kr@gmail.com>
2024-01-11 05:01:28 +0530

    modified:   Dynamic Programming/LIS/length_of_lis.cpp

--- Dynamic Programming/LIS/length_of_lis.cpp
+++ Dynamic Programming/LIS/length_of_lis.cpp
@@ -85,6 +85,28 @@ int longestIncreasingSubsequence(vector<int> &vec, int n)
     return ahead[0];
 }
 
+// Binary Search
+
+int lengthOfLIS(vector<int> &nums)
+{
+    n = nums.size();
+    vector<int> temp;
+    temp.push_back(nums[0]);
+    for (int i = 1; i < n; i++)
+    {
+        if (nums[i] > temp.back())
+        {
+            temp.push_back(nums[i]);
+        }
+        else
+        {
+            int ind = lower_bound(temp.begin(), temp.end(), nums[i]) - temp.begin();
+            temp[ind] = nums[i];
+        }
+    }
+    return temp.size();
+}
+
 int main()
 {
 
