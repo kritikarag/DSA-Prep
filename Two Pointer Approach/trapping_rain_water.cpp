5c14643de7b171a4bc5bed25f5e55ad499932cdc
Kritika Rag <kritikarag.kr@gmail.com>
2024-01-26 05:26:09 +0530

    modified:   Two Pointer Approach/trapping_rain_water.cpp

--- Two Pointer Approach/trapping_rain_water.cpp
+++ Two Pointer Approach/trapping_rain_water.cpp
@@ -1,5 +1,32 @@
 // https://leetcode.com/problems/trapping-rain-water/
 
+#include<bits/stdc++.h>
+using namespace std;
+
+int trap(vector<int> &height)
+{
+    int n = height.size();
+    vector<int> prefix(n), suffix(n);
+
+    prefix[0] = height[0];
+    for (int i = 1; i < n; i++)
+    {
+        prefix[i] = max(prefix[i - 1], height[i]);
+    }
+    suffix[n - 1] = height[n - 1];
+    for (int i = n - 2; i >= 0; i--)
+    {
+        suffix[i] = max(suffix[i + 1], height[i]);
+    }
+
+    int result = 0;
+    for (int i = 0; i < n; i++)
+    {
+        result += min(prefix[i], suffix[i]) - height[i];
+    }
+    return result;
+}
+
 int trap(vector<int> &heights)
 {
     int n = heights.size();
