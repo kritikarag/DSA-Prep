29eeebbc9ec311f61a5a86fcb38778954fb6335c
Kritika Rag <72866402+kritikarag@users.noreply.github.com>
2024-05-01 20:32:19 +0530

    Update balloon_burst.cpp

--- Dynamic Programming/Partition DP/balloon_burst.cpp
+++ Dynamic Programming/Partition DP/balloon_burst.cpp
@@ -21,3 +21,23 @@ int maxCoins(vector<int>& nums) {
     dp.resize(n+1,vector<int>(n+1,-1));
    return solve(1,n,nums); 
 }
+
+int maxCoins(vector<int>& nums) {
+    n = nums.size();
+    nums.insert(nums.begin(),1);
+    nums.push_back(1);
+    dp.resize(n+2,vector<int>(n+2,0));
+    
+    for(int i=n;i>0;i--){
+        for(int j=1;j<=n;j++){
+            if(i>j)continue;
+            int result = 0;
+            for(int k = i; k<=j;k++){
+                result = max(result, nums[k]*nums[i-1]*nums[j+1]+dp[i][k-1]+dp[k+1][j]);
+            }
+            dp[i][j] = result;
+        }
+    }
+    //return solve(1,n,nums); 
+    return dp[1][n];
+}
