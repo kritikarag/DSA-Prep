e91c841e5a8d1621a97ad683b5f197e309793c63
Kritika Rag <72866402+kritikarag@users.noreply.github.com>
2024-05-15 20:04:28 +0530

    Create max_xor_element_with_an_element_from_an_array.cpp

---
+++ Trie/max_xor_element_with_an_element_from_an_array.cpp
@@ -0,0 +1,72 @@
+// https://leetcode.com/problems/maximum-xor-with-an-element-from-array/
+
+struct Node{
+    Node *links[2];
+
+    bool containsKey(int bit){
+        return links[bit]!=NULL;
+    }
+
+    Node* getNode(int bit){
+        return links[bit];
+    }
+
+    void putKey(int bit, Node* node){
+        links[bit] = node;
+    }
+};
+class Solution {
+public:
+    using obj = pair<int,pair<int,int>>;
+    vector<int> maximizeXor(vector<int>& nums, vector<vector<int>>& queries) {
+        int n = nums.size(), q = queries.size();
+        Node *root = new Node();
+        sort(nums.begin(),nums.end());
+        //priority_queue<obj,vector<obj>,greater<obj>>pq;
+        vector<obj>pq;
+        
+        for(int i=0;i<q;i++){
+            pq.push_back({queries[i][1],{queries[i][0],i}});
+        }
+        sort(pq.begin(),pq.end());
+        vector<int>ans(q,-1);
+        int k = 0;
+        for(int i=0;i<q;i++){
+            int limit = pq[i].first;
+            int number = pq[i].second.first;
+            int index = pq[i].second.second;
+
+            while(k<n && nums[k]<=limit){
+                int curr_num = nums[k];
+                Node* node = root;
+                for(int j=10;j>=0;j--){
+                    int bit = (curr_num>>j)&1;
+
+                    if(!node->containsKey(bit)){
+                        node->putKey(bit, new Node());
+                    }
+                    node = node->getNode(bit);
+                }
+                k++;
+            }
+
+            int res = 0;
+
+            Node* node = root;
+
+            for(int j=10;j>=0;j--){
+                int bit = (number>>j);
+
+                if(node->containsKey(1-bit)){
+                    res = res|(1<<j);
+                    node = node->getNode(1-bit);
+                }
+                else node = node->getNode(bit);
+            }
+
+            ans [index] = res;
+        }
+
+        return ans;
+    }
+};
